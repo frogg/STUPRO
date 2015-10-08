82ddb97509bf53721f9daf0ba9744d0b32f81d71
DasGraMMa <info@papa-schlumpf.net>
2015-10-08 12:54:19 +0200

    Added Sphere to View to test.

--- ParaView-TestApp/StuproView/vtkPVStuproView.cxx
+++ ParaView-TestApp/StuproView/vtkPVStuproView.cxx
@@ -1,6 +1,12 @@
 #include "vtkPVStuproView.h"
 #include "vtkObjectFactory.h"
 
+#include "vtkSmartPointer.h"
+#include "vtkSphereSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
+
 vtkStandardNewMacro(vtkPVStuproView);
 //----------------------------------------------------------------------------
 vtkPVStuproView::vtkPVStuproView()
@@ -19,6 +25,22 @@ void vtkPVStuproView::Initialize(unsigned int id)
 	SetGradientBackground(true);
 	SetBackground(255, 0, 0);
 	SetBackground2(0, 0, 255);
+
+	// Create a sphere
+	vtkSmartPointer<vtkSphereSource> sphereSource =
+		vtkSmartPointer<vtkSphereSource>::New();
+	sphereSource->SetCenter(0.0, 0.0, 0.0);
+	sphereSource->SetRadius(5.0);
+
+	vtkSmartPointer<vtkPolyDataMapper> mapper =
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputConnection(sphereSource->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> actor =
+		vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+	GetRenderer()->AddActor(actor);
 }
 
 //----------------------------------------------------------------------------
