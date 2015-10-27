d00bc277563663164262cdfba3cd64edb98e9aa2
Jan Machnik <jmachnik@gmx.de>
2015-10-27 19:17:15 +0100

    Added basic Button, without functionality yet, which will later switch from 3D to 2,5D and vice versa

---
+++ ParaView-TestApp/src/StuproView/GlobeSwitcher.cxx
@@ -0,0 +1,27 @@
+
+
+#include "GlobeSwitcher.h"
+
+#include <QApplication>
+#include <QStyle>
+#include <QMessageBox>
+
+GlobeSwitcher::GlobeSwitcher(QObject* p)
+  : QActionGroup(p)
+{
+  QIcon icon = qApp->style()->standardIcon(QStyle::SP_CustomBase);
+
+  //defines the tooltip and the icon
+  QAction* a = this->addAction(new QAction(icon, "Globeswitcher", this));
+  QObject::connect(a, SIGNAL(triggered(bool)), this, SLOT(onAction()));
+}
+
+GlobeSwitcher::~GlobeSwitcher()
+{
+}
+
+void GlobeSwitcher::onAction()
+{
+  QMessageBox::information(NULL, "Globeswitcher", "Globeswitcher needs to be implemented\n");
+}
+
