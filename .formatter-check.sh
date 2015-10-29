5906cd30870f0551046c4d4d369fb755d4466232
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-29 22:11:01 +0100

    Re-added preprocessor indentation settings; now printing astyle version on check

--- .formatter-check.sh
+++ .formatter-check.sh
@@ -11,6 +11,8 @@ fi
 
 ERROR=0
 
+echo "Running $($COMMAND --version)"
+
 for i in "${FORMATTER_DIRS[@]}"
 do
   output=$($COMMAND --options=.astyle-options-v2 --recursive --dry-run "$i/*.cpp" "$i/*.hpp" "$i/*.?sh" 2>&1 | egrep 'Formatted|command not found')
