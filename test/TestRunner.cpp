e9188de9a7fb975992d9f3b7ed21e89879cc0609
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-29 18:23:53 +0100

    Added automated CppUnit support

---
+++ test/TestRunner.cpp
@@ -0,0 +1,24 @@
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+
+#include <iostream>
+
+using namespace CppUnit;
+
+int main(int argc, char* argv[]) {
+  TextTestRunner runner;
+  TestFactoryRegistry& registry = TestFactoryRegistry::getRegistry();
+
+  // run all tests if none specified on command line
+  Test* testToRun = registry.makeTest();
+  TestSuite* suite = dynamic_cast<TestSuite*>(testToRun);
+
+  if (argc > 1) {
+    testToRun = testToRun->findTest(argv[1]);
+  }
+
+  runner.addTest(testToRun);
+
+  bool failed = runner.run();
+  return !failed;
+}
