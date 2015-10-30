9306c785f81ea9913e93d2236d626cba7cf2b539
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-30 10:29:06 +0100

    Ran formatter on TestRunner class

--- test/TestRunner.cpp
+++ test/TestRunner.cpp
@@ -5,20 +5,22 @@
 
 using namespace CppUnit;
 
-int main(int argc, char* argv[]) {
-  TextTestRunner runner;
-  TestFactoryRegistry& registry = TestFactoryRegistry::getRegistry();
+int main(int argc, char *argv[])
+{
+	TextTestRunner runner;
+	TestFactoryRegistry &registry = TestFactoryRegistry::getRegistry();
 
-  // run all tests if none specified on command line
-  Test* testToRun = registry.makeTest();
-  TestSuite* suite = dynamic_cast<TestSuite*>(testToRun);
+	// run all tests if none specified on command line
+	Test *testToRun = registry.makeTest();
+	TestSuite *suite = dynamic_cast<TestSuite *>(testToRun);
 
-  if (argc > 1) {
-    testToRun = testToRun->findTest(argv[1]);
-  }
+	if (argc > 1)
+	{
+		testToRun = testToRun->findTest(argv[1]);
+	}
 
-  runner.addTest(testToRun);
+	runner.addTest(testToRun);
 
-  bool failed = runner.run();
-  return !failed;
+	bool failed = runner.run();
+	return !failed;
 }
