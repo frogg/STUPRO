07e2fa7529a581761cb326b9087971ab85ed341f
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-30 12:43:41 +0100

    Added dummy implementations for the ImageDownloader class

--- src/ImageDownloader.cpp
+++ src/ImageDownloader.cpp
@@ -1,29 +1,33 @@
 #include "../include/ImageDownloader.hpp"
 
-ImageDownloader::ImageDownloader(tileFetchedCbType imageFetchedCb)
-	: ImageDownloader(imageFetchedCb, NULL) { }
+QNetworkAccessManager ImageDownloader::networkManager;
 
-ImageDownloader::ImageDownloader(tileFetchedCbType imageFetchedCb, QJsonObject *configuration)
-{
+ImageDownloader::ImageDownloader(tileFetchedCbType imageFetchedCb) {
 	this->imageFetchedCb = imageFetchedCb;
 }
 
-ImageDownloader::~ImageDownloader()
-{
+ImageDownloader::ImageDownloader(tileFetchedCbType imageFetchedCb, QJsonObject *configuration) {
+	this->imageFetchedCb = imageFetchedCb;
+	this->config = configuration;
+}
+
+ImageDownloader::~ImageDownloader() {
 
 }
 
-QImage *ImageDownloader::getImage(QString *layer, int zoomLevel, int tileX, int tileY)
-{
-
+QImage *ImageDownloader::getImage(QString *layer, int zoomLevel, int tileX, int tileY) {
+	QMap<QString, QImage> layers;
+	ImageTile tile(layers, zoomLevel, tileX, tileY);
+	this->imageFetchedCb(tile);
 }
 
-QList<QString> ImageDownloader::getAvailableLayers()
-{
-
+QList<QString> ImageDownloader::getAvailableLayers() {
+	QList<QString> availableLayers;
+	availableLayers.append("satellite-imagery");
+	availableLayers.append("heightmap");
+	return availableLayers;
 }
 
-QImage *ImageDownloader::downloadImage(QUrl *imageUrl)
-{
+QImage *ImageDownloader::downloadImage(QUrl *imageUrl) {
 
 }
