49db0ba9a089948aedafb8e90c60a3c9ff5414df
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-30 13:52:05 +0100

    Converted [ ]{2} to \t in ImageDownloader.hpp

--- include/ImageDownloader.hpp
+++ include/ImageDownloader.hpp
@@ -17,64 +17,64 @@
 class ImageDownloader {
 public:
 
-  /**
-   * Type used for the image fetched callback.
-   */
-  typedef std::function<void(ImageTile& tile)> tileFetchedCbType;
+	/**
+	 * Type used for the image fetched callback.
+	 */
+	typedef std::function<void(ImageTile& tile)> tileFetchedCbType;
 
-  /**
-   * Create a new ImageDownloader using the standard configuration.
-   */
-  ImageDownloader(tileFetchedCbType imageFetchedCb);
+	/**
+	 * Create a new ImageDownloader using the standard configuration.
+	 */
+	ImageDownloader(tileFetchedCbType imageFetchedCb);
 
-  /**
-   * Create a new ImageDownloader using the specified configuration.
-   * @param configuration The configuration of this downloader in JSON notation
-   */
-  ImageDownloader(tileFetchedCbType imageFetchedCb, QJsonObject* configuration);
+	/**
+	 * Create a new ImageDownloader using the specified configuration.
+	 * @param configuration The configuration of this downloader in JSON notation
+	 */
+	ImageDownloader(tileFetchedCbType imageFetchedCb, QJsonObject* configuration);
 	~ImageDownloader();
 
-  /**
-   * Get an image tile of a data layer.
-   * @param layer Unique identifier of an existing layer. Refer to the method
-   * getAvailableLayers() for a list of possible layers.
-   * @param zoomLevel Zoom level down the quad tree of images
-   * @param tileX Horizontal position of the requested tile
-   * @param tileY Vertical position of the requested tile
-   * @return The requested image tile as a QImage pointer
-   */
-  QImage* getImage(QString* layer, int zoomLevel, int tileX, int tileY);
+	/**
+	 * Get an image tile of a data layer.
+	 * @param layer Unique identifier of an existing layer. Refer to the method
+	 * getAvailableLayers() for a list of possible layers.
+	 * @param zoomLevel Zoom level down the quad tree of images
+	 * @param tileX Horizontal position of the requested tile
+	 * @param tileY Vertical position of the requested tile
+	 * @return The requested image tile as a QImage pointer
+	 */
+	QImage* getImage(QString* layer, int zoomLevel, int tileX, int tileY);
 
-  /**
-   * Get a list of available image layers.
-   * @return A list of all available image layers as specified by the
-   * configuration file
-   */
-  QList<QString> getAvailableLayers();
+	/**
+	 * Get a list of available image layers.
+	 * @return A list of all available image layers as specified by the
+	 * configuration file
+	 */
+	QList<QString> getAvailableLayers();
 
 private:
-  /**
-   * A JSON object holding all configuration information.
-   */
-  QJsonObject *config;
+	/**
+	 * A JSON object holding all configuration information.
+	 */
+	QJsonObject *config;
 
-  /**
-   * Callback function to be called when a requested tile was downloaded or fetched from the file
-   * system.
-   */
-  tileFetchedCbType imageFetchedCb;
+	/**
+	 * Callback function to be called when a requested tile was downloaded or fetched from the file
+	 * system.
+	 */
+	tileFetchedCbType imageFetchedCb;
 
-  /**
-   * Network access manager used to make requests to the image API.
-   */
-  static QNetworkAccessManager networkManager;
+	/**
+	 * Network access manager used to make requests to the image API.
+	 */
+	static QNetworkAccessManager networkManager;
 
-  /**
-   * Download an image from a specified URL.
-   * @param imageUrl URL of the image
-   * @return The image downloaded from the specified URL
-   */
-  QImage* downloadImage(QUrl* imageUrl);
+	/**
+	 * Download an image from a specified URL.
+	 * @param imageUrl URL of the image
+	 * @return The image downloaded from the specified URL
+	 */
+	QImage* downloadImage(QUrl* imageUrl);
 
 };
 
