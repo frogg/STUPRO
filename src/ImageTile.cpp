fefaac0b45aba0e95d98d1efcaf077f279ee5f94
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-30 10:28:52 +0100

    Ran formatter on ImageTile class

--- src/ImageTile.cpp
+++ src/ImageTile.cpp
@@ -5,45 +5,55 @@
 #include <QList>
 #include <QMap>
 
-ImageTile::ImageTile(const QMap<QString, QImage>& layers, int zoomLevel, int tileX, int tileY) {
-  this->setLayers(layers);
-  this->setZoomLevel(zoomLevel);
-  this->setTileX(tileX);
-  this->setTileY(tileY);
+ImageTile::ImageTile(const QMap<QString, QImage> &layers, int zoomLevel, int tileX, int tileY)
+{
+	this->setLayers(layers);
+	this->setZoomLevel(zoomLevel);
+	this->setTileX(tileX);
+	this->setTileY(tileY);
 }
 
-ImageTile::~ImageTile() {
-  this->layers.clear();
+ImageTile::~ImageTile()
+{
+	this->layers.clear();
 }
 
-QMap<QString, QImage>& ImageTile::getLayers() {
-  return this->layers;
+QMap<QString, QImage> &ImageTile::getLayers()
+{
+	return this->layers;
 }
 
-void ImageTile::setLayers(const QMap<QString, QImage>& layers) {
-  this->layers = layers;
+void ImageTile::setLayers(const QMap<QString, QImage> &layers)
+{
+	this->layers = layers;
 }
 
-int ImageTile::getZoomLevel() {
-  return this->zoomLevel;
+int ImageTile::getZoomLevel()
+{
+	return this->zoomLevel;
 }
 
-void ImageTile::setZoomLevel(int zoomLevel) {
-  this->zoomLevel = zoomLevel;
+void ImageTile::setZoomLevel(int zoomLevel)
+{
+	this->zoomLevel = zoomLevel;
 }
 
-int ImageTile::getTileX() {
-  return this->tileX;
+int ImageTile::getTileX()
+{
+	return this->tileX;
 }
 
-void ImageTile::setTileX(int tileX) {
-  this->tileX = tileX;
+void ImageTile::setTileX(int tileX)
+{
+	this->tileX = tileX;
 }
 
-int ImageTile::getTileY() {
-  return this->tileY;
+int ImageTile::getTileY()
+{
+	return this->tileY;
 }
 
-void ImageTile::setTileY(int tileY) {
-  this->tileY = tileY;
+void ImageTile::setTileY(int tileY)
+{
+	this->tileY = tileY;
 }
