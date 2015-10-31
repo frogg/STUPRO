77720b7a67da2773a56d467cf845311bd6e6bc02
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-31 11:53:33 +0100

    Got the CppUnit tests for the ImageDownloader class working as expected

--- test/util/TestImageDownloader.cpp
+++ test/util/TestImageDownloader.cpp
@@ -1,6 +1,9 @@
 #include "TestImageDownloader.hpp"
 
+#include <QApplication>
 #include <ImageTile.hpp>
+#include <iostream>
+#include <future>
 
 void TestImageDownloader::testGetAvailableLayers() {
 	ImageDownloader downloader([](ImageTile & tile) {
@@ -9,19 +12,25 @@ void TestImageDownloader::testGetAvailableLayers() {
 
 	QList<QString> availableLayers = downloader.getAvailableLayers();
 
-	CPPUNIT_ASSERT(TRUE);
-	// CPPUNIT_ASSERT(availableLayers.contains("satellite-imagery"));
-	// CPPUNIT_ASSERT(availableLayers.contains("heightmap"));
+	CPPUNIT_ASSERT(availableLayers.contains("satellite-imagery"));
+	CPPUNIT_ASSERT(availableLayers.contains("heightmap"));
 }
 
 void TestImageDownloader::testGetTile() {
-	ImageDownloader downloader([](ImageTile & tile) {
-		// TODO: somehow make cppunit wait until the callback completes
-		CPPUNIT_ASSERT(tile.getZoomLevel() == 0);
-		CPPUNIT_ASSERT(tile.getTileX() == 0);
-		CPPUNIT_ASSERT(tile.getTileY() == 0);
+	std::promise<ImageTile> promise;
+	std::future<ImageTile> future = promise.get_future();
+
+	ImageDownloader downloader([&](ImageTile &tile) {
+		promise.set_value(tile);
 	});
 
 	QString layerName = downloader.getAvailableLayers()[0];
 	downloader.getTile(layerName, 0, 0, 0);
+
+	future.wait();
+	ImageTile tile = future.get();
+
+	CPPUNIT_ASSERT(tile.getZoomLevel() == 0);
+	CPPUNIT_ASSERT(tile.getTileX() == 0);
+	CPPUNIT_ASSERT(tile.getTileY() == 0);
 }
