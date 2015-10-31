fa6769f73f8492c9e4b68d34c698fa800629e390
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-31 14:06:23 +0100

    Updated interface of ImageDownloader to work with the new MetaImages, added some helper functions

--- include/ImageDownloader.hpp
+++ include/ImageDownloader.hpp
@@ -4,9 +4,10 @@
 #include "ImageTile.hpp"
 #include "ImageCache.hpp"
 #include "ImageLayerDescription.hpp"
+#include "MetaImage.hpp"
 
+#include <exception>
 #include <functional>
-#include <QImage>
 #include <QFile>
 #include <QUrl>
 #include <QNetworkAccessManager>
@@ -15,48 +16,122 @@
 #include <QString>
 #include <QMap>
 
+struct InvalidTileLocationException : public std::exception { };
+
+struct InvalidTileZoomException : public InvalidTileLocationException {
+	int givenZoomLevel;
+	int minZoomLevel;
+	int maxZoomLevel;
+
+	InvalidTileZoomException(int given, int min, int max)
+		: givenZoomLevel(given), minZoomLevel(min), maxZoomLevel(max) { }
+
+	const char * what() const throw() {
+		QString message("The given zoomLevel is invalid. Expected value between (including) %1 and %2. Was given %3.");
+		message = message.arg(minZoomLevel, maxZoomLevel, givenZoomLevel);
+
+		return message.toStdString().c_str();
+	}
+};
+
+struct InvalidTilePosition : public InvalidTileLocationException {
+	int zoomLevel;
+	int xMin, xMax, xActual;
+	int yMin, yMax, yActual;
+
+	InvalidTilePosition(int zoomLevel, int xMin, int xMax, int xActual, int yMin, int yMax, int yActual)
+		: zoomLevel(zoomLevel), xMin(xMin), xMax(xMax), xActual(xActual),
+		  yMin(yMin), yMax(yMax), yActual(yActual) { }
+
+	const char * what() const throw() {
+		QString message("The given tile position is invalid. Valid tile positions for zoom level %1 must be within x%2 to x%3 and y%5 to y%6. Was given x%4 y%7.");
+		// message = message.arg(QString::number(zoomLevel), xMin, xMax, xActual, yMin, yMax, yActual);
+		message = message.arg(zoomLevel);
+		message = message.arg(xMin, xMax, xActual);
+		message = message.arg(yMin, yMax, yActual);
+
+		return message.toStdString().c_str();
+	}
+};
+
 class ImageDownloader {
 public:
-
 	/**
 	 * Type used for the tile fetched callback.
 	 */
-	typedef std::function<void(ImageTile& tile)> tileFetchedCbType;
+	typedef std::function<void(ImageTile& tile)> TileFetchedCb;
+
+	static const int MIN_ZOOM_LEVEL = 0;
+	static const int MAX_ZOOM_LEVEL = 15;
+	static const int WIDTH_ON_MIN_ZOOM = 8;
+	static const int HEIGHT_ON_MIN_ZOOM = 4;
 
 	/**
-	 * Create a new ImageDownloader using the standard configuration.
+	 * Creates a new ImageDownloader using the default configuration.
+	 *
+	 * @param tileFetchedCb the callback to call whenever a tile has finished loading.
 	 */
-	ImageDownloader(tileFetchedCbType tileFetchedCb);
+	ImageDownloader(TileFetchedCb tileFetchedCb);
 
 	/**
-	 * Create a new ImageDownloader using the specified configuration.
-	 * @param configuration The configuration of this downloader in JSON notation
+	 * Creates a new ImageDownloader using the given configuration.
+	 *
+	 * @param tileFetchedCb the callback to call whenever a tile has finished loading.
+	 * @param imageLayers   a map containing layername - layerdescription objects to be used for
+	 *                      loading the tiles.
 	 */
-	ImageDownloader(tileFetchedCbType tileFetchedCb, QMap<QString, ImageLayerDescription> imageLayers);
+	ImageDownloader(TileFetchedCb tileFetchedCb, QMap<QString, ImageLayerDescription> imageLayers);
 	~ImageDownloader();
 
+	/**
+	 * Fetches images of all layers at the given location.
+	 * The tile fetched callback given to the constructor will be called as soon as all images are
+	 * loaded.
+	 *
+	 * @param zoomLevel how deep to dive into the quad-tree
+	 * @param tileX     horizontal position of the requested tile (westernmost tile = 0)
+	 * @param tileY     vertical position of the requested tile (northernmost tile = 0)
+	 */
 	void getTile(int zoomLevel, int tileX, int tileY);
+
+	/**
+	 * Fetches the image of the given layer at the given location.
+	 * @see {#getAvailableLayers} for a list of all available layers. The tile fetched callback
+	 * given to the constructor will be called as soon as all images are loaded.
+	 *
+	 * @param layer     the layer to get the image from
+	 * @param zoomLevel how deep to dive into the quad-tree
+	 * @param tileX     horizontal position of the requested tile (westernmost tile = 0)
+	 * @param tileY     vertical position of the requested tile (northernmost tile = 0)
+	 */
 	void getTile(QString &layer, int zoomLevel, int tileX, int tileY);
 
 	/**
-	 * Get an image tile of a data layer.
-	 * @param layer Unique identifier of an existing layer. Refer to the method
-	 * getAvailableLayers() for a list of possible layers.
-	 * @param zoomLevel Zoom level down the quad tree of images
-	 * @param tileX Horizontal position of the requested tile
-	 * @param tileY Vertical position of the requested tile
-	 * @return The requested image tile as a QImage pointer
+	 * Fetches the images of the given layers at the given location.
+	 * @see {#getAvailableLayers} for a list of all available layers. The tile fetched callback
+	 * given to the constructor will be called as soon as all images are loaded.
+	 *
+	 * @param layers    the layers to get the images from
+	 * @param zoomLevel how deep to dive into the quad-tree
+	 * @param tileX     horizontal position of the requested tile (westernmost tile = 0)
+	 * @param tileY     vertical position of the requested tile (northernmost tile = 0)
 	 */
 	void getTile(QList<QString> layers, int zoomLevel, int tileX, int tileY);
 
 	/**
-	 * Get a list of available image layers.
-	 * @return A list of all available image layers as specified by the
-	 * configuration file
+	 * Returns a list of available image layers.
+	 *
+	 * @returns a list containing the names of all available image layers as read from the config
+	 *          file
 	 */
 	QList<QString> getAvailableLayers();
 
 private:
+	/**
+	 * Type used for the image downloaded callback.
+	 */
+	typedef std::function<void(MetaImage)> ImageDownloadedCb;
+
 	/**
 	 * An object holding all configuration information.
 	 */
@@ -66,7 +141,7 @@ private:
 	 * Callback function to be called when a requested tile was downloaded or fetched from the file
 	 * system.
 	 */
-	tileFetchedCbType tileFetchedCb;
+	TileFetchedCb tileFetchedCb;
 
 	/**
 	 * Network access manager used to make requests to the image API.
@@ -74,18 +149,48 @@ private:
 	static QNetworkAccessManager networkManager;
 
 	/**
-	 * Image cache used to save and retrieve downloaded tiles.
+	 * Chechs if the given tile location is valid for the underlying quad-tree.
+	 *
+	 * @param zoomLevel how deep to dive into the quad-tree
+	 * @param tileX     horizontal position of the requested tile (westernmost tile = 0)
+	 * @param tileY     vertical position of the requested tile (northernmost tile = 0)
+	 *
+	 * @throws InvalidTileZoomException if the zoom level is out of bounds
+	 * @throws InvalidTilePosition      if the x or y position of the tile is out of bounds for the
+	 *                                  given zoom level
 	 */
-	static ImageCache imageCache;
+	static void validateTileLocation(int zoomLevel, int tileX, int tileY);
 
-	QString buildTileDownloadUrl(QString layer, int zoomLevel, int tileX, int tileY);
+	/**
+	 * Calculates the bounding box in lat-long from the given tile location.
+	 *
+	 * @param zoomLevel how deep to dive into the quad-tree
+	 * @param tileX     horizontal position of the requested tile (westernmost tile = 0)
+	 * @param tileY     vertical position of the requested tile (northernmost tile = 0)
+	 *
+	 * @returns a string representation of the lat-long bounding box
+	 */
+	static QString calculateBoundingBox(int zoomLevel, int tileX, int tileY);
+
+	/**
+	 * Returns the URL at which the image with the given properties can be found.
+	 *
+	 * @param layer     the layer from which to get the image
+	 * @param zoomLevel how deep to dive into the quad-tree
+	 * @param tileX     horizontal position of the requested tile (westernmost tile = 0)
+	 * @param tileY     vertical position of the requested tile (northernmost tile = 0)
+	 *
+	 * @returns a URL pointing to the image with the given properties
+	 */
+	QUrl buildTileDownloadUrl(QString layer, int zoomLevel, int tileX, int tileY);
 
 	/**
 	 * Download an image from a specified URL.
+	 *
 	 * @param imageUrl URL of the image
 	 * @return The image downloaded from the specified URL
 	 */
-	QImage downloadImageSync(QUrl& imageUrl);
+	void downloadImage(QUrl &imageUrl, ImageDownloadedCb imageDownloadedCb);
 };
 
 #endif
--- src/ImageDownloader.cpp
+++ src/ImageDownloader.cpp
@@ -1,18 +1,17 @@
 #include "../include/ImageDownloader.hpp"
 #include "../include/ConfigUtil.hpp"
 
-#include <QQueue>
+#include <QImage>
 #include <thread>
 
 QNetworkAccessManager ImageDownloader::networkManager;
-ImageCache ImageDownloader::imageCache;
 
-ImageDownloader::ImageDownloader(tileFetchedCbType tileFetchedCb) {
+ImageDownloader::ImageDownloader(TileFetchedCb tileFetchedCb) {
 	this->tileFetchedCb = tileFetchedCb;
 	this->imageLayers = ConfigUtil::loadConfigFile("./src/res/layers.cfg");
 }
 
-ImageDownloader::ImageDownloader(tileFetchedCbType tileFetchedCb,
+ImageDownloader::ImageDownloader(TileFetchedCb tileFetchedCb,
 								 QMap<QString, ImageLayerDescription> imageLayers) {
 	this->tileFetchedCb = tileFetchedCb;
 	this->imageLayers = imageLayers;
@@ -36,20 +35,23 @@ void ImageDownloader::getTile(QList<QString> layers, int zoomLevel, int tileX, i
 
 	// create a thread that kicks off all image requests, waits until all are completed and
 	// calls the tileFetchedCb with the results
-	std::thread fetchThread([&]() {
+	std::thread fetchThread([ = ]() {
 		// build up a queue
-		QQueue<QString> layerFetchQueue;
-		foreach (QString layer, layers) {
-			layerFetchQueue.enqueue(layer);
+		QMap<QString, MetaImage> fetchedTiles;
+
+		foreach (QString layerName, layers) {
+			// if (ImageCache::isImageCached(layerName, zoomLevel, tileX, tileY)) {
+			// 	QImage tile = ImageCache::getCachedImage(layerName, zoomLevel, tileX, tileY);
+			// 	fetchedTiles.insert(layerName, tile);
+			// } else {
+			// 	// trigger the download of the image
+			// }
 		}
 
-		QMap<QString, QImage> layers;
-		while (!layerFetchQueue.isEmpty()) {
-
-		}
-		ImageTile tile(layers, zoomLevel, tileX, tileY);
+		ImageTile tile(fetchedTiles, zoomLevel, tileX, tileY);
 		this->tileFetchedCb(tile);
 	});
+	// detach the thread to keep it running until all downloads are completed
 	fetchThread.detach();
 }
 
@@ -57,12 +59,41 @@ QList<QString> ImageDownloader::getAvailableLayers() {
 	return this->imageLayers.keys();
 }
 
-QString ImageDownloader::buildTileDownloadUrl(QString layer, int zoomLevel, int tileX, int tileY) {
-	//
+void ImageDownloader::validateTileLocation(int zoomLevel, int tileX, int tileY) {
+	// check zoom level range
+	if (zoomLevel < MIN_ZOOM_LEVEL || zoomLevel > MAX_ZOOM_LEVEL) {
+		throw InvalidTileZoomException(zoomLevel, 0, 15);
+	}
+
 }
 
-QImage ImageDownloader::downloadImageSync(QUrl &imageUrl) {
+QString ImageDownloader::calculateBoundingBox(int zoomLevel, int tileX, int tileY) {
+	return QString("");
+}
+
+QUrl ImageDownloader::buildTileDownloadUrl(QString layer, int zoomLevel, int tileX, int tileY) {
+	// check if the given location is valid, throw an exception otherwise
+	validateTileLocation(zoomLevel, tileX, tileY);
+
+	ImageLayerDescription layerDescription = this->imageLayers[layer];
+	QString urlString = layerDescription.baseUrl;
+
+	// add format parameters
+	urlString += "&format=" + layerDescription.format;
+
+	// add tile size parameters
+	urlString += "&width=" + layerDescription.tileSize;
+	urlString += "&height=" + layerDescription.tileSize;
+
+	// add bounding box parameters
+	urlString += "&bbox=" + calculateBoundingBox(zoomLevel, tileX, tileY);
+
+	return QUrl(urlString);
+}
+
+void ImageDownloader::downloadImage(QUrl &imageUrl, ImageDownloadedCb imageDownloadedCb) {
 	QImage dummyImage(512, 512, QImage::Format_RGB32);
+	MetaImage dummyMetaImage(dummyImage, 0, 0);
 	// TODO: actually download the image
-	return dummyImage;
+	imageDownloadedCb(dummyImage);
 }
