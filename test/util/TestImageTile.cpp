17a03f48618ff98413e50fada34b4f36a53db80e
Jonas Auer <jonas.auer.94@gmail.com>
2015-10-31 14:35:30 +0100

    Modified ImageTile test to work with the updated ImageTile class

--- test/util/TestImageTile.cpp
+++ test/util/TestImageTile.cpp
@@ -3,7 +3,7 @@
 #include <QDebug>
 
 void TestImageTile::testMetaData() {
-	QMap<QString, QImage> layers;
+	QMap<QString, MetaImage *> layers;
 	ImageTile tile(layers, 10, 2, 3);
 
 	CPPUNIT_ASSERT(tile.getZoomLevel() == 10);
@@ -12,19 +12,23 @@ void TestImageTile::testMetaData() {
 }
 
 void TestImageTile::testLayers() {
-	QMap<QString, QImage> layers;
+	QMap<QString, MetaImage *> layers;
 	QImage imageOne(512, 512, QImage::Format_RGB32);
+	MetaImage metaImageOne(imageOne);
 	QImage imageTwo(512, 512, QImage::Format_RGB32);
-	layers.insert("layerOne", imageOne);
-	layers.insert("layerTwo", imageTwo);
+	MetaImage metaImageTwo(imageTwo);
+	layers.insert("layerOne", &metaImageOne);
+	layers.insert("layerTwo", &metaImageTwo);
 	ImageTile tile(layers, 10, 2, 3);
 
-	CPPUNIT_ASSERT(tile.getLayers().size() == 2);
-	CPPUNIT_ASSERT(tile.getLayers().contains("layerOne"));
-	CPPUNIT_ASSERT(tile.getLayers().contains("layerTwo"));
-	CPPUNIT_ASSERT(tile.getLayers().contains("layerTwo"));
-	CPPUNIT_ASSERT(tile.getLayers().value("layerOne").width() == 512
-				   && tile.getLayers().value("layerOne").height() == 512);
-	CPPUNIT_ASSERT(tile.getLayers().value("layerTwo").width() == 512
-				   && tile.getLayers().value("layerTwo").height() == 512);
+	QMap<QString, MetaImage *> layerTiles = tile.getLayers();
+	CPPUNIT_ASSERT(layerTiles.size() == 2);
+	CPPUNIT_ASSERT(layerTiles.contains("layerOne"));
+	CPPUNIT_ASSERT(layerTiles.contains("layerTwo"));
+	MetaImage *layerOne = layerTiles["layerOne"];
+	CPPUNIT_ASSERT(layerOne->getImage().width() == 512
+				   && layerOne->getImage().height() == 512);
+	MetaImage *layerTwo = layerTiles["layerTwo"];
+	CPPUNIT_ASSERT(layerTwo->getImage().width() == 512
+				   && layerTwo->getImage().height() == 512);
 }
