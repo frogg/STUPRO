2e0c18f60a69316b1ee641baafaacefb34d9ca35
Jonas Auer <jonas.auer.94@gmail.com>
2015-11-02 23:06:19 +0100

    Added simple logging class for debugging thread-related issues

---
+++ include/DebugLogger.hpp
@@ -0,0 +1,12 @@
+#ifndef KRONOS_DEBUGLOGGER_HPP
+#define KRONOS_DEBUGLOGGER_HPP
+
+#include <iostream>
+#include <QString>
+
+class DebugLogger {
+public:
+    static void debug(QString message);
+};
+
+#endif
