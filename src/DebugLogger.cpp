68dd7534b82e7cece9e74648dadcb4590035c5ee
Jonas Auer <jonas.auer.94@gmail.com>
2015-11-04 09:51:01 +0100

    Removed DebugLogger class

--- src/DebugLogger.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/DebugLogger.hpp"
-
-#include <thread>
-
-using namespace std;
-
-void DebugLogger::debug(QString message) {
-	cout << std::this_thread::get_id() << ": " << message.toStdString().c_str() << endl;
-}
