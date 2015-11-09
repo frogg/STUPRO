9c9d65fa4e6ad49d90e5cad15062e7b5c0cc9d61
Jonas Auer <jonas.auer.94@gmail.com>
2015-11-09 22:29:10 +0100

    Modified astyle installation script

--- scripts/download-and-install-astyle.sh
+++ scripts/download-and-install-astyle.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-mkdir -p ./bin/astyle
-cd ./bin/astyle
-$(wget -O astyle_2.05.1_linux.tar.gz http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.05.1/astyle_2.05.1_linux.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fastyle%2Ffiles%2Fastyle%2Fastyle%25202.05.1%2F&ts=1446294940&use_mirror=skylink)
+ASTYLE_DIR="./bin/astyle"
+ASTYLE_SRC_URL="http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.05.1/astyle_2.05.1_linux.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fastyle%2Ffiles%2Fastyle%2Fastyle%25202.05.1%2F&ts=1446294940&use_mirror=skylink"
+
+ROOT_DIR=$PWD
+ABS_ASTYLE_DIR=$PWD/$ASTYLE_DIR
+
+mkdir -p $ABS_ASTYLE_DIR
+cd $ABS_ASTYLE_DIR
+
+$(wget -O astyle_2.05.1_linux.tar.gz $ASTYLE_SRC_URL)
 tar zxf astyle_2.05.1_linux.tar.gz
-cd astyle/build/gcc/
+cd ./astyle/build/gcc/
 make release -j 4
 sudo make install
-cd ../../../../..
+
+cd $ROOT_DIR
