f8ef7044902c3d62068a547c0848f358a26c7967
Jonas Auer <jonas.auer.94@gmail.com>
2015-11-10 09:53:39 +0100

    Kronos build script now exits with error code if cmake or make fails

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,17 @@
 mkdir -p bin
 cd bin
 cmake ../
+
+if [ $? != 0 ]; then
+  echo "Aborting due to errors executing cmake"
+  exit 1
+fi
+
 make -j 4
+
+if [ $? != 0 ]; then
+  echo "Aborting due to errors while building kronos"
+  exit 1
+fi
+
 ctest --output-on-failure
