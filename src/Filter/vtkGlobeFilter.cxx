3ebb0da362acd84989bf25756c3f1ae909783465
Paul D <dieterpl@informatik.uni-stuttgart.de>
2015-11-24 15:37:13 +0100

    I added some sample Code for manipulating data in the filter

--- src/Filter/vtkGlobeFilter.cxx
+++ src/Filter/vtkGlobeFilter.cxx
@@ -16,15 +16,20 @@
 
 #include "vtkCell.h"
 #include "vtkCellData.h"
-#include "vtkDataSet.h"
+#include "vtkPolyData.h"
 #include "vtkDoubleArray.h"
+#include "vtkCellArray.h"
+#include "vtkFloatArray.h"
 #include "vtkGenericCell.h"
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
 #include "vtkPointData.h"
 #include "vtkTensor.h"
-
+#include "vtkPolyData.h"
+#include <vtkCellArray.h>
+#include "vtkPointData.h"
+#include "vtkPoints.h"
 #include <math.h>
 
 vtkStandardNewMacro(vtkGlobeFilter);
@@ -32,6 +37,7 @@ vtkStandardNewMacro(vtkGlobeFilter);
 vtkGlobeFilter::vtkGlobeFilter()
 {
   std::cout << "Hello" << endl;
+
 }
 
 int vtkGlobeFilter::RequestData(
@@ -39,18 +45,59 @@ int vtkGlobeFilter::RequestData(
   vtkInformationVector **inputVector,
   vtkInformationVector *outputVector)
 {
+      std::cout << "dataRequested" << endl;
   // get the info objects
 vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
 vtkInformation *outInfo = outputVector->GetInformationObject(0);
 
 // get the input and output
-vtkDataSet *input = vtkDataSet::SafeDownCast(
+vtkPolyData *input = vtkPolyData::SafeDownCast(
   inInfo->Get(vtkDataObject::DATA_OBJECT()));
-vtkDataSet *output = vtkDataSet::SafeDownCast(
+vtkPolyData *output = vtkPolyData::SafeDownCast(
   outInfo->Get(vtkDataObject::DATA_OBJECT()));
 
-  output->CopyStructure( input );
-  return 1;
+    int nmbPoints=100;
+
+    vtkCellArray *newPolys;
+    newPolys = vtkCellArray::New();
+    newPolys->Allocate(newPolys->EstimateSize(nmbPoints*nmbPoints, 3));
+    
+    vtkPoints *newPoints;
+    newPoints = vtkPoints::New();
+    vtkFloatArray *newNormals;
+    newPoints->Allocate(nmbPoints*nmbPoints);
+    newNormals = vtkFloatArray::New();
+    newNormals->SetNumberOfComponents(3);
+    newNormals->Allocate(3*nmbPoints*nmbPoints);
+    newNormals->SetName("Normals");
+    newPoints->InsertNextPoint(1, 0.0, 0.0);
+    newNormals->InsertTuple3(1, 0.0, 0.0, 0.0);
+    vtkIdType pts[4];
+    for (int i=0; i<nmbPoints; i++) {
+        for (int j=0 ; j<nmbPoints; j++) {
+            pts[0]=j;
+            pts[1]=i;
+            pts[2]=0.0;
+            newPolys->InsertNextCell(3, pts);
+        }
+    }
+
+
+  
+  //output->CopyStructure( input );
+    newPoints->Squeeze();
+    output->SetPoints(newPoints);
+    newPoints->Delete();
+    
+    newNormals->Squeeze();
+    output->GetPointData()->SetNormals(newNormals);
+    newNormals->Delete();
+    
+    newPolys->Squeeze();
+    output->SetPolys(newPolys);
+    newPolys->Delete();
+
+    return 1;
 }
 
 const char *vtkGlobeFilter::GetVectorModeAsString(void)
