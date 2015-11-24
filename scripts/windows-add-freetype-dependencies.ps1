e9d7125a815e599ce74d5be9513543552561540c
DasGraMMa <info@papa-schlumpf.net>
2015-11-24 15:54:38 +0100

    Moved (and renamed) script for windows, enabled tests for Windows again

--- scripts/windows-add-freetype-dependencies.ps1
+++ scripts/windows-add-freetype-dependencies.ps1
@@ -1,9 +1,9 @@
-if(!(Test-Path bin/* -include CMakeCache.txt)) {
+if(!(Test-Path ../bin/* -include CMakeCache.txt)) {
 	break
 }
 
-$ParaViewDir = (Get-Content bin/CMakeCache.txt) | Select-String -pattern "ParaView_DIR"
+$ParaViewDir = (Get-Content ../bin/CMakeCache.txt) | Select-String -pattern "ParaView_DIR"
 $ParaViewDir = $ParaViewDir.ToString().Replace("ParaView_DIR:PATH=", "").Replace("/", "\")
 $ParaViewDir = "$ParaViewDir\lib\Debug"
 $FreeTypeDirs = "$ParaViewDir\vtkRenderingFreeTypeOpenGLCS-pv4.3.lib;$ParaViewDir\vtkRenderingFreeTypeCS-pv4.3.lib;$ParaViewDir\vtkRenderingFreeTypeOpenGL-pv4.3.lib;$ParaViewDir\vtkRenderingFreeType-pv4.3.lib;"
-(Get-Content bin/src/paraview_stupro.vcxproj) | ForEach-Object { $_ -replace "<AdditionalDependencies>", "<AdditionalDependencies>$FreeTypeDirs" } | Set-Content bin/src/paraview_stupro.vcxproj
+(Get-Content ../bin/src/paraview_stupro.vcxproj) | ForEach-Object { $_ -replace "<AdditionalDependencies>", "<AdditionalDependencies>$FreeTypeDirs" } | Set-Content ../bin/src/paraview_stupro.vcxproj
