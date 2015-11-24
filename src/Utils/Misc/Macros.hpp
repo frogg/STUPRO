c84effb390d878f6cd5485293364d1da36e5ab2e
Marukyu <marukyu@marubbq>
2015-11-24 16:34:09 +0100

    Created basic Macro definition file with OS and NoThrow macros

---
+++ src/Utils/Misc/Macros.hpp
@@ -0,0 +1,33 @@
+#ifndef KRONOS_MACROS_HPP
+#define KRONOS_MACROS_HPP
+
+/**
+ * Operation system macros.
+ * 
+ * Can be used to conditionally compile platform-specific code.
+ */
+#if defined(_WIN32)
+#	define KRONOS_WINDOWS
+#elif defined(__APPLE__)
+#	define KRONOS_MACOS
+#elif defined(__linux__)
+#	define KRONOS_LINUX
+#elif
+#	error Unrecognized Operating System!
+#endif
+
+
+/**
+ * KRONOS_NOTHROW Macro.
+ * 
+ * Usable at the end of a function declaration to guarantee that it will never throw exceptions.
+ */
+#if defined(KRONOS_WINDOWS)
+	// Visual Studio 2013 doesn't support noexcept, so use a regular empty throw specifier instead.
+#	define KRONOS_NOTHROW throw()
+#else
+	// G++ and Clang support noexcept, so use it instead of the deprecated throw() notation.
+#	define KRONOS_NOTHROW noexcept
+#endif
+
+#endif
