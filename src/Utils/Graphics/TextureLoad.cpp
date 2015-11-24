179d08933755a51d7563466072c2785ee4580648
Marukyu <marukyu@marubbq>
2015-11-24 12:38:55 +0100

    Improved readability of TextureLoad

--- src/Utils/Graphics/TextureLoad.cpp
+++ src/Utils/Graphics/TextureLoad.cpp
@@ -17,41 +17,57 @@
 
 vtkSmartPointer<vtkOpenGLTexture> loadAlphaTexture(const QImage& rgb, const QImage& alpha)
 {
+	// Index for each color channel in an RGBA image.
+	static const int CHANNEL_RED = 0;
+	static const int CHANNEL_GREEN = 1;
+	static const int CHANNEL_BLUE = 2;
+	static const int CHANNEL_ALPHA = 3;
+	
+	// Number of channels in an RGBA image.
+	static const int CHANNEL_COUNT = 4;
+	
+	// Create a VTK image to load the RGBA data into.
 	vtkSmartPointer<vtkImageData> vtkimage = vtkImageData::New();
 
-	int width = rgb.width();
-	int height = rgb.height();
+	// Store size of RGB input image.
+	unsigned int width = rgb.width();
+	unsigned int height = rgb.height();
 
-	if (width != alpha.width() || height != alpha.height())
+	// Compare sizes of the RGB and alpha input image. If there is a mismatch, the texture cannot be loaded.
+	if (width != (unsigned int)alpha.width() || height != (unsigned int)alpha.height())
 	{
-		throw std::runtime_error((
-		        "RGB (" + std::to_string(width) + "x" + std::to_string(height) + ") and Alpha ("
-		                + std::to_string(alpha.width()) + "x" + std::to_string(alpha.height())
-		                + ") Texture sizes mismatch").c_str());
+		throw std::runtime_error(QString("RGB (%1x%2) and Alpha (%3x%4) texture sizes mismatch")
+				.arg(width).arg(height).arg(alpha.width()).arg(alpha.height()));
 	}
 
+	// Initialize parameters for vtkImageData.
 	vtkimage->SetExtent(0, width - 1, 0, height - 1, 0, 0);
 	vtkimage->SetSpacing(1.0, 1.0, 1.0);
 	vtkimage->SetOrigin(0.0, 0.0, 0.0);
-	vtkimage->AllocateScalars(VTK_UNSIGNED_CHAR, 4);
+	vtkimage->AllocateScalars(VTK_UNSIGNED_CHAR, CHANNEL_COUNT);
 
-	for (int i = 0; i < height; i++)
+	// Iterate over image rows.
+	for (unsigned int y = 0; y < height; y++)
 	{
-		unsigned char* row;
-		row = static_cast<unsigned char*>(vtkimage->GetScalarPointer(0, height - i - 1, 0));
+		// Get pointer to current target row. Image data will be written to this pointer.
+		unsigned char * targetPixels = static_cast<unsigned char*>(vtkimage->GetScalarPointer(0, height - y - 1, 0));
 
-		const QRgb* linePixelsRgb = reinterpret_cast<const QRgb*>(rgb.scanLine(i));
-		const QRgb* linePixelsAlpha = reinterpret_cast<const QRgb*>(alpha.scanLine(i));
+		// Get pointers to current source rows. Image data will be read from these pointers.
+		const QRgb* sourcePixelsRgb = reinterpret_cast<const QRgb*>(rgb.scanLine(y));
+		const QRgb* sourcePixelsAlpha = reinterpret_cast<const QRgb*>(alpha.scanLine(y));
 
-		for (int j = 0; j < width; j++)
+		// Iterate over image columns.
+		for (unsigned int x = 0; x < width; x++)
 		{
-			const QRgb& colRgb = linePixelsRgb[j];
-			const QRgb& colAlpha = linePixelsAlpha[j];
+			// Get current pixel from each of the source rows.
+			const QRgb& pxRgb = sourcePixelsRgb[x];
+			const QRgb& pxAlpha = sourcePixelsAlpha[x];
 
-			row[j * 4] = qRed(colRgb);
-			row[j * 4 + 1] = qGreen(colRgb);
-			row[j * 4 + 2] = qBlue(colRgb);
-			row[j * 4 + 3] = qRed(colAlpha);
+			// Combine components from both source rows into target row. Component order is RGBA.
+			targetPixels[x * CHANNEL_COUNT + CHANNEL_RED] = qRed(pxRgb);
+			targetPixels[x * CHANNEL_COUNT + CHANNEL_GREEN] = qGreen(pxRgb);
+			targetPixels[x * CHANNEL_COUNT + CHANNEL_BLUE] = qBlue(pxRgb);
+			targetPixels[x * CHANNEL_COUNT + CHANNEL_ALPHA] = qRed(pxAlpha);
 		}
 	}
 
--- src/Utils/Graphics/TextureLoad.hpp
+++ src/Utils/Graphics/TextureLoad.hpp
@@ -13,6 +13,17 @@
 
 class QImage;
 
+/**
+ * Creates a VTK texture with an alpha channel from an RGB image and an image whose red channel will serve as the alpha
+ * channel for the created texture.
+ * 
+ * @param rgb
+ *        Contains the RGB image data to be loaded into the texture.
+ * @param alpha
+ *        Contains the Alpha channel data (read from the Red component) to be loaded into the texture.
+ * 
+ * @return A smart pointer to a texture holding the combined image.
+ */
 vtkSmartPointer<vtkOpenGLTexture> loadAlphaTexture(const QImage& rgb, const QImage& alpha);
 
 #endif /* TEXTURELOAD_HPP_ */
