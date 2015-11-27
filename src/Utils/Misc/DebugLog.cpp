fa98d4ee6d49a99ece5af5077c081ef90092f811
Marukyu <glados@cirno.de>
2015-11-27 15:11:44 +0100

    Added Debug Logging functionality

---
+++ src/Utils/Misc/DebugLog.cpp
@@ -0,0 +1,20 @@
+#include <Utils/Misc/DebugLog.hpp>
+
+const std::string DebugLog::DEBUG_LOGFILE = "debug.log";
+
+DebugLog * DebugLog::instance = nullptr;
+
+DebugLog & DebugLog::get()
+{
+	if (instance == nullptr)
+	{
+		instance = new DebugLog;
+	}
+	
+	return *instance;
+}
+
+DebugLog::DebugLog() : std::ofstream(DEBUG_LOGFILE.c_str(), std::ios::trunc)
+{
+}
+
---
+++ src/Utils/Misc/DebugLog.hpp
@@ -0,0 +1,30 @@
+#ifndef SRC_UTILS_MISC_DEBUGLOG_HPP_
+#define SRC_UTILS_MISC_DEBUGLOG_HPP_
+
+/**
+ * Utility class for debug logging. Use like a standard output stream (cout).
+ * 
+ * Writes all data to a file called "debug.log".
+ */
+class DebugLog : public std::ofstream
+{
+public:
+
+	/**
+	 * The name of the file the stream writes to.
+	 */
+	static const std::string DEBUG_LOGFILE;
+	
+	/**
+	 * Creates the debug log instance and file if it does not exist yet and returns the stream.
+	 */
+	static DebugLog & get();
+	
+private:
+	
+	DebugLog();
+	
+	static DebugLog * instance;
+};
+
+#endif /* SRC_UTILS_MISC_DEBUGLOG_HPP_ */
