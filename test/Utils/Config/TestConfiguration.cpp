d4a36c7887f9cf551ced2462756c8a6648c7705d
Jonas Auer <jonas.auer.94@gmail.com>
2015-12-01 11:00:07 +0100

    Moved Configuration Tests to GTest

--- test/Utils/Config/TestConfiguration.cpp
+++ test/Utils/Config/TestConfiguration.cpp
@@ -1,52 +1,50 @@
-#include "TestConfiguration.hpp"
-
-#include <cppunit/TestAssert.h>
+#include <gtest/gtest.h>
 #include <QString>
 #include <string>
 #include <Utils/Config/Configuration.hpp>
 
-void TestConfiguration::testValidCalls() {
-	CPPUNIT_ASSERT(Configuration::getInstance().hasKey("globe.radius"));
-	CPPUNIT_ASSERT(!Configuration::getInstance().hasKey("globe.invalid"));
+TEST(TestConfiguration, ValidCalls) {
+	EXPECT_TRUE(Configuration::getInstance().hasKey("globe.radius"));
+	EXPECT_TRUE(!Configuration::getInstance().hasKey("globe.invalid"));
 
-	CPPUNIT_ASSERT_EQUAL(
+	EXPECT_EQ(
 		42,
 		Configuration::getInstance().getInteger("globe.radius")
 	);
 
-	CPPUNIT_ASSERT_EQUAL(
+	EXPECT_EQ(
 		std::string("Foo"),
 		Configuration::getInstance().getString("globe.name").toStdString()
 	);
 
-	CPPUNIT_ASSERT_EQUAL(
+	EXPECT_EQ(
 		true,
 		Configuration::getInstance().getBoolean("globe.spheric")
 	);
 
-	CPPUNIT_ASSERT_EQUAL(
+	EXPECT_EQ(
 		12.2345,
 		Configuration::getInstance().getDouble("latitude")
 	);
 
-	CPPUNIT_ASSERT_EQUAL(
+	EXPECT_EQ(
 		12.2345f,
 		Configuration::getInstance().getFloat("latitude")
 	);
 
-	CPPUNIT_ASSERT_EQUAL(
+	EXPECT_EQ(
 		5.234,
 		Configuration::getInstance().getDouble("deeply.nested.configuration.group.longitude")
 	);
 }
 
-void TestConfiguration::testInvalidCalls() {
-	CPPUNIT_ASSERT_THROW(
+TEST(TestConfiguration, InvalidCalls) {
+	EXPECT_THROW(
 		Configuration::getInstance().getString("globe.invalid"),
 		InvalidKeyException
 	);
 
-	CPPUNIT_ASSERT_THROW(
+	EXPECT_THROW(
 		Configuration::getInstance().getDouble("globe.name"),
 		InvalidValueException
 	);
