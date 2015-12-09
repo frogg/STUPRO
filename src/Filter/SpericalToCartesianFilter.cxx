54e28ccdee5044430fd5febfb65b63294575853c
Tobias Beeh <Tobias.Beeh@t-online.de>
2015-12-09 13:00:37 +0100

    First step towards a working filter

--- src/Filter/SpericalToCartesianFilter.cxx
+++ src/Filter/SpericalToCartesianFilter.cxx
@@ -17,14 +17,14 @@
 #include "vtkCellArray.h"
 #include "vtkFloatArray.h"
 #include "vtkPointData.h"
+#include "vtkCellIterator.h"
 
 #include <math.h>
 
 // Still the same everytime
-vtkStandardNewMacro(SpericalToCartesianFilter);
+vtkStandardNewMacro(SpericalToCartesianFilter)
 
-int SpericalToCartesianFilter::RequestData(
-                                            vtkInformation *vtkNotUsed(request),
+int SpericalToCartesianFilter::RequestData(vtkInformation *vtkNotUsed(request),
                                             vtkInformationVector **inputVector,
                                             vtkInformationVector *outputVector) {
     
@@ -33,76 +33,22 @@ int SpericalToCartesianFilter::RequestData(
     vtkInformation *outInfo = outputVector->GetInformationObject(0);
     
     // Cast the input- and output-vectors to something useful.
-    vtkDataSet *input = vtkDataSet::SafeDownCast(
-                                                 inInfo->Get(vtkDataObject::DATA_OBJECT()));
-  //  vtkDataSet *output = vtkDataSet::SafeDownCast(
-   //                                               outInfo->Get(vtkDataObject::DATA_OBJECT()));
+    vtkDataSet *input = vtkDataSet::SafeDownCast(inInfo->Get(vtkDataObject::DATA_OBJECT()));
     
-    vtkPolyData *output = vtkPolyData::SafeDownCast(
-                                                    outInfo->Get(vtkDataObject::DATA_OBJECT()));
+    vtkPolyData *output = vtkPolyData::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
     // For now: use the exact input as output.
-    
-    double coodinate[3];
-    for(int i=0; i<input->GetNumberOfPoints(); i++){
-        input->GetPoint(i,coodinate);//->SetData;
-        vtkWarningMacro(<< coodinate[0] << ";" << coodinate[1] << ";" << coodinate[2]  << "number of points"
-                        );
-        //coodinate[0] = 1000; //doesn't work
-    }
-    //points defining a cell
-    vtkWarningMacro(<< input->GetNumberOfCells()  << "number of cells");
-
     output->CopyStructure(input);
-   // output->GetPointData()->GetNumberOfArrays();
-    //SetTuble
-    vtkPointData* pointData = output->GetPointData();
-    pointData->GetNumberOfArrays()
-    
-    vtkWarningMacro(<< output->GetPointData()->GetVectors()->GetArrayType()  << "arraytype");
-    //CellData,FieldData
-    /*
-    int nmbPoints=4;
-    
-    vtkCellArray *newPolys;
-    newPolys = vtkCellArray::New();
-    newPolys->Allocate(newPolys->EstimateSize(nmbPoints, 3));
-    
-    vtkPoints *newPoints;
-    newPoints = vtkPoints::New();
-    vtkFloatArray *newNormals;
-    newPoints->Allocate(nmbPoints);
-    newNormals = vtkFloatArray::New();
-    newNormals->SetNumberOfComponents(3);
-    newNormals->Allocate(3*nmbPoints);
-    newNormals->SetName("Normals");
-    vtkIdType pts[4];
-    
-    for(int i=0; i<input->GetNumberOfPoints()/3; i++){
-        input->GetPoint(i,coodinate);
-        pts[0]  = newPoints->InsertNextPoint(coodinate[0]*2,coodinate[1]*2,coodinate[2]);
-        input->GetPoint(i+1,coodinate);
-        pts[1]  = newPoints->InsertNextPoint(coodinate[0]*2,coodinate[1]*2,coodinate[2]);
-        input->GetPoint(i+2,coodinate);
-        pts[2]  = newPoints->InsertNextPoint(coodinate[0]*2,coodinate[1]*2,coodinate[2]);
-        newPolys->InsertNextCell(3, pts);
+
+    vtkPoints *points = output->GetPoints();
+    for(int i=0; i<points->GetNumberOfPoints(); i++){
+        double *coord = points->GetPoint(i);
+        for (int j = 0; j < 3; j++) {
+            std::cout << "Set Point " << coord[j] << " to " << coord[j] * 2 << std::endl;
+            coord[j] = 2 * coord[j];
+        }
+        points->SetPoint(i, coord);
     }
-     newPoints->Squeeze();
-    output->SetPoints(newPoints);
-    newPoints->Delete();
-    */
- /*   newNormals->Squeeze();
-    output->GetPointData()->SetNormals(newNormals);
-    newNormals->Delete();
-*/
-    /*
-    newPolys->Squeeze();
-    output->SetPolys(newPolys);
-    newPolys->Delete();
-*/
-    /*--test */
-    vtkWarningMacro(<< " CALLED RequestData IN FLOW FILTER");
-    
-    
+
     return 1;
 }
 
