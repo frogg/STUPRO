be77804d7e1c3d25dda0ae47c8f779f20d48e26a
Frederik Riedel <f@polarapps.de>
2015-12-14 15:51:15 +0100

    added NonTemporalDataPoint which holds information for non-temporal data

---
+++ src/Reader/DataReader/NonTemporalDataPoint.cpp
@@ -0,0 +1,9 @@
+//
+//  NonTemporalDataPoint.cpp
+//  DataPointThings
+//
+//  Created by Frederik Riedel on 14.12.15.
+//  Copyright © 2015 Quappi. All rights reserved.
+//
+
+#include "NonTemporalDataPoint.hpp"
