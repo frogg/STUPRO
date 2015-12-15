be9968d26cfd9e463fd957556b318d9cf75b738e
Jan Tagscherer <jan.tagscherer@gmail.com>
2015-12-15 12:16:55 +0100

    Added a header file for the AbstractJsonReader

---
+++ src/Reader/DataReader/AbstractJsonReader.hpp
@@ -0,0 +1,54 @@
+#ifndef KRONOS_ABSTRACT_JSON_READER_HPP
+#define KRONOS_ABSTRACT_JSON_READER_HPP
+
+#include <QMap>
+#include <vtkPolyData.h>
+
+class AbstractJsonReader {
+public:
+    /**
+     * Create a new AbstractJsonReader from a map of data from a JSON file
+     * @param data A potentially nested map of data from the JSON file
+     */
+    AbstractJsonReader(QMap data);
+    virtual ~AbstractJsonReader() { }
+
+    /**
+     * Get the type of data read by this reader
+     * @return The type of data read by this reader
+     */
+    int getDataType();
+
+    /**
+     * Check whether the data read by this reader is of temporal nature
+     * @return True if each data point has time information, false otherwise
+     */
+    bool hasTemporalData();
+
+    /**
+     * Get all data stored in the file this reader uses, pruned by a specified zoom level.
+     * @param zoomLevel The zoom level of the data set. Every data point with a higher zoom level
+     * will be discarded while creating the vtkPolyData.
+     * @return All data relevant to the specified zoom level, formatted as vtkPolyData with each
+     * point's data stored in the data point's scalar values.
+     */
+    vtkPolyData getVtkDataSet(int zoomLevel);
+private:
+    /**
+     * Boolean flag denoting whether the data read by this reader contains time information
+     */
+    bool temporal;
+
+    /**
+     * Integer denoting the type of data read by this reader, using constants defined in the
+     * DataType.hpp file
+     */
+    int dataType;
+
+    /**
+     * A set of all points and their stored information read by this reader
+     */
+    PointDataSet pointDataSet;
+};
+
+#endif
