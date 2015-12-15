9db04dd7a4aaf67adf2ff8d5b79535583a8b34ae
Jan Tagscherer <jan.tagscherer@gmail.com>
2015-12-15 15:45:01 +0100

    Added some exceptions to the header file

--- src/Reader/DataReader/JsonReaderFactory.hpp
+++ src/Reader/DataReader/JsonReaderFactory.hpp
@@ -3,6 +3,43 @@
 
 #include <Reader/DataReader/AbstractJsonReader.hpp>
 
+#include <exception>
+
+/**
+ * Super class for all exceptions thrown while a JSON file is being loaded.
+ */
+struct ReaderException : public std::exception {
+	std::string reason;
+
+	ReaderException(QString reason) : reason(reason.toStdString()) { }
+
+	const char *what() const KRONOS_NOTHROW override {
+		return reason.c_str();
+	}
+};
+
+/**
+ * Exception thrown if the JSON file could not be opened.
+ */
+struct JsonFileOpenException : public ReaderException {
+	JsonFileOpenException(QString path, QString errorDescription)
+			: ReaderException(
+				QString("The JSON file at '%1' could not be opened: %2")
+				.arg(path).arg(errorDescription)
+			) { }
+};
+
+/**
+ * Exception thrown if the content of the JSON file could not be parsed.
+ */
+struct JsonParseException : public ReaderException {
+	JsonParseException(QString path, QString errorDescription)
+			: ReaderException(
+				QString("The JSON file at '%1' could not be parsed: %2")
+				.arg(path).arg(errorDescription)
+			) { }
+};
+
 class JsonReaderFactory {
 public:
     /**
@@ -11,7 +48,7 @@ public:
      * @return A JSON reader for the given file that will handle the file's contents with respect
      * to its meta information
      */
-    static AbstractJsonReader createReader(QString filename);
+    static AbstractJsonReader createReader(const QString filename) const;
 };
 
 #endif
