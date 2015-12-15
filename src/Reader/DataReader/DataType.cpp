65fb11de0b795a3d74d76ed22ab1d3fa30bfaacb
Jan Tagscherer <jan.tagscherer@gmail.com>
2015-12-15 20:29:17 +0100

    Added a DataType implementation to make its constants better usable

---
+++ src/Reader/DataReader/DataType.cpp
@@ -0,0 +1,8 @@
+#include <Reader/DataReader/DataType.hpp>
+
+const int DataType::CITIES;
+const int DataType::FLIGHTS;
+const int DataType::TWEETS;
+const int DataType::PRECIPITATION;
+const int DataType::TEMPERATURE;
+const int DataType::WIND;
