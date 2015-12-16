52ba6124415914a4adc3f3db85ba7b4cd458f87b
Larissa Laich <la8895@web.de>
2015-12-16 15:42:32 +0100

    didnot find mistake in own implementation, maybe different dregees than in online calculator

--- test/Filter/TestSphericalToCartesianFilter.cpp
+++ test/Filter/TestSphericalToCartesianFilter.cpp
@@ -5,35 +5,80 @@
 #include <iostream>
 
 
+// write further tests with http://www.random-science-tools.com/maths/coordinate-converter.htm
 TEST(TestSphericalToCartesianFilter, ValidCalls) {
     vtkSmartPointer<SpericalToCartesianFilter> filter = vtkSmartPointer<SpericalToCartesianFilter>::New();
     double points[3];
     points[0] = 0;
     points[1] = 0;
     points[2] = 0;
-
     
-    double result[3];
-    
-    filter->transformToCartesian(result,200);
+    filter->transformToCartesian(points,200);
 
-
-    EXPECT_TRUE(std::abs(result[1]) < 0.01);
-    EXPECT_TRUE(std::abs(result[1]) < 0.01);
-    EXPECT_TRUE(std::abs(result[0]-200.0) < 0.01);
-    
-    std::cout<<result[0] << ":   x" << std::endl;
-    std::cout<<result[1] << ":   y" << std::endl;
-    std::cout<<result[2] << ":   z" << std::endl;
+    EXPECT_TRUE(std::abs(points[0]) < 0.01);
+    EXPECT_TRUE(std::abs(points[1]) < 0.01);
+    EXPECT_TRUE(std::abs(points[2]-200.0) < 0.01);
     
     points[0] = 90;
     points[1] = 0;
     points[2] = 0;
-    filter->transformToCartesian(result,200);
+    filter->transformToCartesian(points,300);
 
-    std::cout<<result[0] << ":   x" << std::endl;
-    std::cout<<result[1] << ":   y" << std::endl;
-    std::cout<<result[2] << ":   z" << std::endl;
+    EXPECT_TRUE(std::abs(points[0]-300.0) < 0.01);
+    EXPECT_TRUE(std::abs(points[1]) < 0.01);
+    EXPECT_TRUE(std::abs(points[2]) < 0.01);
+
+    points[0] = 0;
+    points[1] = 90;
+    points[2] = 10;
+    filter->transformToCartesian(points,100);
+    
+    EXPECT_TRUE(std::abs(points[0]) < 0.01);
+    EXPECT_TRUE(std::abs(points[1]-110.0) < 0.01);
+    EXPECT_TRUE(std::abs(points[2]) < 0.01);
+    
+    //negative angles
+    points[0] = -90;
+    points[1] = 0;
+    points[2] = 0;
+    filter->transformToCartesian(points,3);
+    EXPECT_TRUE(std::abs(points[0]+3) < 0.01);
+    EXPECT_TRUE(std::abs(points[1]) < 0.01);
+    EXPECT_TRUE(std::abs(points[2]) < 0.01);
+
+    /*
+    points[0] = 45;
+    points[1] = 45;
+    points[2] = 0;
+    filter->transformToCartesian(points,30);
+
+    EXPECT_TRUE(std::abs(points[0]-15) < 0.01);
+    EXPECT_TRUE(std::abs(points[1]-15) < 0.01);
+    EXPECT_TRUE(std::abs(points[2]-21.21) < 0.1);
+    
+    
+    std::cout<<points[0] << ":   x" << std::endl;
+    std::cout<<points[1] << ":   y" << std::endl;
+    std::cout<<points[2] << ":   z" << std::endl;
+
+    points[0] = 120;
+    points[1] = 60;
+    points[2] = 30;
+    filter->transformToCartesian(points,0);
+    
+    EXPECT_TRUE(std::abs(points[0]+64.95) < 0.01);
+    EXPECT_TRUE(std::abs(points[1]-112.5) < 0.01);
+    EXPECT_TRUE(std::abs(points[2]-75.00) < 0.1);
+
+
+    
+    std::cout<<points[0] << ":   x" << std::endl;
+    std::cout<<points[1] << ":   y" << std::endl;
+    std::cout<<points[2] << ":   z" << std::endl;
+    */
+
+
+
 
 //    EXPECT_TRUE(std::abs(result[0]-100) < 0.01);
  //   EXPECT_TRUE(std::abs(result[0]-100) < 0.01);
@@ -56,6 +101,12 @@ TEST(TestSphericalToCartesianFilter, ValidCalls) {
 }
 
 TEST(SphericalToCartesianFilter, InvalidCalls) {
+  /*  double points[3];
+    points[0] = -135;
+    points[1] = 125.264;
+    points[2] = 0;
+    filter->transformToCartesian(points,51.9615);
+    */
     /*
     EXPECT_THROW(
                  Configuration::getInstance().getString("globe.invalid"),
