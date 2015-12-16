fd34e3fdd69b77054a1c2552e686d9b157a74c2c
Frederik Riedel <f@polarapps.de>
2015-12-16 15:53:06 +0100

    Schrott

--- src/Reader/DataReader/JsonReader.cpp
+++ src/Reader/DataReader/JsonReader.cpp
@@ -1,6 +1,10 @@
 #include "JsonReader.hpp"
 #include <vtkSmartPointer.h>
 #include <vtkDataArray.h>
+#include <vtkFloatArray.h>
+#include <vtkPointData.h>
+#include <vtkDataArray.h>
+#include <vtkCharArray.h>
 #include <Reader/DataReader/DataType.hpp>
 
 JsonReader::JsonReader(rapidjson::Value& jsonDocument, int dataType, bool temporal) : dataType(dataType), temporal(temporal) {
@@ -12,7 +16,7 @@ JsonReader::JsonReader(rapidjson::Value& jsonDocument, int dataType, bool tempor
         case DataType::CITIES:
             //Cities
             break;
-
+            
         case DataType::FLIGHTS:
             //Flights
             break;
@@ -52,18 +56,78 @@ bool JsonReader::hasTemporalData() const {
 
 vtkPolyData JsonReader::getVtkDataSet(int zoomLevel) {
     
-    this->pointDataSet.getDataPoints();
     
-    /*
-    vtkPolyData* polydata = vtkPolyData::New();
+    switch (this->dataType) {
+            
+        case DataType::CITIES:
+            //Cities
+            break;
+            
+        case DataType::FLIGHTS:
+            //Flights
+            break;
+            
+        case DataType::TWEETS:
+            //Tweets
+            break;
+            
+        case DataType::PRECIPITATION:
+            //PRECIPITATION
+            break;
+            
+        case DataType::TEMPERATURE:
+            //TEMPERATURE
+            break;
+            
+        case DataType::WIND:
+            //WIND
+            break;
+            
+            
+        default:
+            break;
+    }
+    
 
-    vtkPoints* points = vtkPoints::New();
     
-    vtkDataArray* dataArray = vtkDataArray::New();
     
-    points->SetData(dataArray);
+    vtkSmartPointer<vtkPolyData> dataSet = vtkSmartPointer<vtkPolyData>::New();
     
-    polydata->SetPoints(points);
+    vtkSmartPointer<vtkPoints> newPoints = vtkSmartPointer<vtkPoints>::New();
     
-    return polydata;*/
+    vtkSmartPointer<vtkCharArray> newScalars = vtkSmartPointer<vtkCharArray>::New();
+    
+    newScalars->SetNumberOfComponents(1);
+    newScalars->SetName("Name");
+    
+    
+    QList<DataPoint> dataPoints = this->pointDataSet.getDataPoints();
+    
+    
+    QList<DataPoint>::iterator iterator;
+    
+    for(iterator = dataPoints.begin();iterator!=dataPoints.end();iterator++) {
+        
+        //all prioritys which are less or eqla to the zoomLevel will be written into the vtkPolyData
+        if(iterator->getPriority() <= zoomLevel) {
+            
+            //add point to vtkPoints newPoints
+            newPoints->InsertNextPoint(iterator->getCoordinate().lat(), iterator->getCoordinate().lon(), 0);
+            
+            //newScalars->InsertNextValue(iterator->get)
+        }
+        
+        
+        
+    }
+    
+    
+    dataSet->SetPoints(newPoints);
+    dataSet->GetPointData()->SetScalars(newScalars);
+    dataSet->GetPointData()->GetScalars("Name");
+    
+    //hier vielleicht den Datentyp übergeben
+    dataSet->SetActiveAttributeInfo(<#vtkInformation *info#>, <#int fieldAssociation#>, <#int attributeType#>, <#const char *name#>, <#int arrayType#>, <#int numComponents#>, <#int numTuples#>)
+    
+    return dataSet;
 }
