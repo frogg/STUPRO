4a02e516f0f0bdd8499147e3d14f3ee928fce1dd
Jan Tagscherer <jan.tagscherer@gmail.com>
2015-12-18 14:57:09 +0100

    Added some tests for the data point parsing

--- test/Reader/DataReader/TestJsonReader.cpp
+++ test/Reader/DataReader/TestJsonReader.cpp
@@ -5,7 +5,23 @@
 #include <Reader/DataReader/JsonReaderFactory.hpp>
 #include <Reader/DataReader/JsonReader.hpp>
 #include <Reader/DataReader/DataType.hpp>
+#include <Reader/DataReader/DataPoints/NonTemporalDataPoints/CityDataPoint.hpp>
 
 TEST(TestJsonReader, ReadCityData) {
 	JsonReader cityReader = JsonReaderFactory::createReader("res/test-data/cities.json");
+	
+	EXPECT_EQ(
+		cityReader.pointDataSet.getDataPoints().size(),
+		4
+	);
+	
+	const CityDataPoint& firstDataPoint = static_cast<const CityDataPoint&>(
+		cityReader.pointDataSet.getDataPoints().at(1)
+	);
+	
+	
+	EXPECT_EQ(
+		firstDataPoint.getName().toStdString(),
+		"Los Angeles"
+	);
 }
