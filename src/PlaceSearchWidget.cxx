a663db635282bb004eb4db766a2446abb2fded26
Jonas Auer <jonas.auer.94@gmail.com>
2016-01-08 14:44:46 +0100

    Added search icon to the search widget's search button

--- src/PlaceSearchWidget.cxx
+++ src/PlaceSearchWidget.cxx
@@ -3,6 +3,8 @@
 #include <QHBoxLayout>
 #include <QLineEdit>
 #include <QPushButton>
+#include <QIcon>
+#include <QSize>
 
 #include <iostream>
 
@@ -10,12 +12,15 @@ PlaceSearchWidget::PlaceSearchWidget(QWidget* parent)
         : QWidget(parent) {
     QHBoxLayout *layout = new QHBoxLayout(this);
     layout->setMargin(0);
+    layout->setSpacing(2);
 
     this->searchBar = new QLineEdit(this);
     QObject::connect(this->searchBar, SIGNAL(returnPressed()), this, SLOT(startSearch()));
     layout->addWidget(this->searchBar);
 
     QPushButton* searchButton = new QPushButton(this);
+    searchButton->setIcon(QIcon("./res/icons/search.png"));
+    searchButton->setIconSize(QSize(16, 16));
     QObject::connect(searchButton, SIGNAL(clicked()), this, SLOT(startSearch()));
     layout->addWidget(searchButton);
 }
