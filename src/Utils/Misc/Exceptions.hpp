c410618cc11739f83854361c6e781be0a227487a
Jonas Auer <jonas.auer.94@gmail.com>
2016-01-22 12:15:38 +0100

    Added header file containing useful general exceptions

---
+++ src/Utils/Misc/Exceptions.hpp
@@ -0,0 +1,17 @@
+
+#ifndef KRONOS_UTILS_MISC_EXCEPTIONS_HPP
+#define KRONOS_UTILS_MISC_EXCEPTIONS_HPP
+
+#include <Utils/Misc/Macros.hpp>
+
+struct InvalidStateException : public std::exception {
+    std::string reason;
+
+	InvalidStateException(QString reason) : reason(reason.toStdString()) { }
+
+	const char *what() const KRONOS_NOTHROW override{
+		return reason.c_str();
+	}
+};
+
+#endif
