09f8260a48f0e925ee6ff2168df0e67877603c34
Jonas Auer <jonas.auer.94@gmail.com>
2016-01-22 15:37:15 +0100

    Modified city SQL query to match all results caseinsensitive and added exception handling

--- src/Utils/Database/CitiesDatabase.cpp
+++ src/Utils/Database/CitiesDatabase.cpp
@@ -28,26 +28,28 @@ void CitiesDatabase::printAllCities(){
 }
 
 void CitiesDatabase::getCity(std::string name, std::vector<City> *cities){
+    try {
+        /* Create SQL statement */
+        string sql = "SELECT * from CITY where LOWER(NAME) like LOWER('%"+ name +"%') ORDER BY CHAR_LENGTH(NAME), NAME DESC;";
 
-    /* Create SQL statement */
-    string sql = "SELECT * from CITY where NAME like '%"+ name +"%'";
+        /* Create a non-transactional object. */
+        nontransaction N(*this->dbConnection);
 
-    /* Create a non-transactional object. */
-    nontransaction N(*this->dbConnection);
+        /* Execute SQL query */
+        result R( N.exec( sql ));
 
-    /* Execute SQL query */
-    result R( N.exec( sql ));
-
-    /* List down all the records */
-    for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-        City result;
-        result.name = c[1].as<string>();
-        result.countryCode = c[2].as<string>();
-        result.latitude = c[3].as<float>();
-        result.longitude = c[4].as<float>();
-        //store result in vector
-        cities->push_back(result);
+        /* List down all the records */
+        for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+            City result;
+            result.name = c[1].as<string>();
+            result.countryCode = c[2].as<string>();
+            result.latitude = c[3].as<float>();
+            result.longitude = c[4].as<float>();
+            //store result in vector
+            cities->push_back(result);
+        }
+        //cout << "Operation done successfully, getCity" << endl;
+    } catch (pqxx::data_exception e) {
+        cout << "Error executing SQL query:" << std::endl << e.what() << std::endl;
     }
-    //cout << "Operation done successfully, getCity" << endl;
-
 }
