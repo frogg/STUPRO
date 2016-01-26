006dcf4109dea6c4cfd7ae65edfa122475606e0f
Gravetow <gravetobackup@gmail.com>
2016-01-26 12:55:01 +0100

    Removed reinitializations of constants, that caused build errors on Windows

--- src/Reader/DataReader/DataType.cpp
+++ src/Reader/DataReader/DataType.cpp
@@ -1,13 +1,5 @@
 #include <Reader/DataReader/DataType.hpp>
 
-const int DataType::CITIES;
-const int DataType::FLIGHTS;
-const int DataType::TWEETS;
-const int DataType::PRECIPITATION;
-const int DataType::TEMPERATURE;
-const int DataType::WIND;
-const int DataType::CLOUDCOVER;
-
 bool DataType::isTemporal(const int dataType) {
     return (
         dataType == DataType::TWEETS || dataType == DataType::PRECIPITATION ||
