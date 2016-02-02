e990c1d703cb9846782eb9e6a8c0f45625398cd1
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-02 19:54:25 +0100

    Increased the precipitation type value by one to workaround what seems to be a bug in VTK

--- src/Reader/DataReader/PolyDataSetHelper.cpp
+++ src/Reader/DataReader/PolyDataSetHelper.cpp
@@ -317,8 +317,11 @@ vtkSmartPointer<vtkPolyData> PolyDataSetHelper::createPolyDataSet(
 			};
 			precipitationRates->SetTuple(tupleNumber, precipitationRate);
 
+            // Increase the actual precipitation type by one since ParaView's selection extraction
+            // seems to have a bug with array values of zero. This makes precipitation values be
+            // in the range of 1 to 5.
 			double precipitationType[1] = {
-				(double) dataPoint->getPrecipitationType()
+				(double) dataPoint->getPrecipitationType() + 1
 			};
 			precipitationTypes->SetTuple(tupleNumber, precipitationType);
 
