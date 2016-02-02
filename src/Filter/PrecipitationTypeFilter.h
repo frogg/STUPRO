c99fd5bdfe6dee443477386a8e281f6cf7843e92
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-02 20:09:32 +0100

    Added documentation comments to the header file

--- src/Filter/PrecipitationTypeFilter.h
+++ src/Filter/PrecipitationTypeFilter.h
@@ -24,24 +24,68 @@ public:
 	int FillOutputPortInformation(int port, vtkInformation *info) override;
 	int FillInputPortInformation(int port, vtkInformation *info) override;
 	
+    /**
+     * Callback method for setting the visibility of precipitation of undefined type.
+     * @param enabled `1` if this precipitation type should be visible, `0` if it should be hidden
+     */
 	void enableUndefined(int enabled);
+    
+    /**
+     * Callback method for setting the visibility of precipitation of type rain.
+     * @param enabled `1` if this precipitation type should be visible, `0` if it should be hidden
+     */
 	void enableRain(int enabled);
+    
+    /**
+     * Callback method for setting the visibility of precipitation of type snow.
+     * @param enabled `1` if this precipitation type should be visible, `0` if it should be hidden
+     */
 	void enableSnow(int enabled);
+    
+    /**
+     * Callback method for setting the visibility of precipitation of type sleet.
+     * @param enabled `1` if this precipitation type should be visible, `0` if it should be hidden
+     */
 	void enableSleet(int enabled);
+    
+    /**
+     * Callback method for setting the visibility of precipitation of type hail.
+     * @param enabled `1` if this precipitation type should be visible, `0` if it should be hidden
+     */
 	void enableHail(int enabled);
 
 private:
+    /**
+     * Initialize a new instance of the precipitation type filter.
+     */
 	PrecipitationTypeFilter();
 	~PrecipitationTypeFilter();
 
 	PrecipitationTypeFilter(const PrecipitationTypeFilter &); // Not implemented.
     void operator=(const PrecipitationTypeFilter &); // Not implemented.
     
+    /**
+     * Internal method that handles VTK-related mechanics to set a specific precipitation type's visibility.
+     * @param type The precipitation type whose visibility should be changed
+     * @param display True if the precipitation type should be displayed, false otherwise
+     */
     void displayPrecipitationType(PrecipitationDataPoint::PrecipitationType type, bool display);
 	
+    /**
+     * The `vtkSelection` that is being used internally to prune the data set.
+     */
 	vtkSmartPointer<vtkSelection> selection;
+    
+    /**
+     * The `vtkSelectionNode` that is attached to the `vtkSelection` this class also holds.
+     * This node contains information on which precipitation types should be kept and will be updated accordingly when callbacks are called.
+     */
 	vtkSmartPointer<vtkSelectionNode> selectionNode;
 	
+    /**
+     * An internal data structure that maps each precipitation type to a boolean value that denotes its visibility.
+     * This will be updated on callbacks and used to build necessary VTK structures.
+     */
 	QMap<PrecipitationDataPoint::PrecipitationType, bool> precipitationTypeVisibilities;
 };
 
