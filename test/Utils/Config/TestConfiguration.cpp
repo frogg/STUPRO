05f4ff9b1893cb595a46d267a6e3aaa49d35aaf1
Tobias Beeh <Tobias.Beeh@t-online.de>
2016-02-05 12:49:21 +0100

    changed a test back that i misunderstood

--- test/Utils/Config/TestConfiguration.cpp
+++ test/Utils/Config/TestConfiguration.cpp
@@ -4,23 +4,48 @@
 #include <Utils/Config/Configuration.hpp>
 
 TEST(TestConfiguration, ValidCalls) {
-	EXPECT_TRUE(Configuration::getInstance().hasKey("globe.radius"));
-	EXPECT_TRUE(!Configuration::getInstance().hasKey("globe.invalid"));
+    EXPECT_TRUE(Configuration::getInstance().hasKey("globe.radius"));
+    EXPECT_TRUE(!Configuration::getInstance().hasKey("globe.invalid"));
 
-	EXPECT_EQ(
-	    100.0,
-	    Configuration::getInstance().getDouble("globe.radius")
-	);
+    EXPECT_EQ(
+        100,
+        Configuration::getInstance().getDouble("globe.radius")
+    );
+
+    EXPECT_EQ(
+        std::string("Foo"),
+        Configuration::getInstance().getString("globe.name").toStdString()
+    );
+
+    EXPECT_EQ(
+        true,
+        Configuration::getInstance().getBoolean("globe.spheric")
+    );
+
+    EXPECT_EQ(
+        12.2345,
+        Configuration::getInstance().getDouble("latitude")
+    );
+
+    EXPECT_EQ(
+        12.2345f,
+        Configuration::getInstance().getFloat("latitude")
+    );
+
+    EXPECT_EQ(
+        5.234,
+        Configuration::getInstance().getDouble("deeply.nested.configuration.group.longitude")
+    );
 }
 
 TEST(TestConfiguration, InvalidCalls) {
-	EXPECT_THROW(
-	    Configuration::getInstance().getString("globe.invalid"),
-	    InvalidKeyException
-	);
+    EXPECT_THROW(
+        Configuration::getInstance().getString("globe.invalid"),
+        InvalidKeyException
+    );
 
-	EXPECT_THROW(
-	    Configuration::getInstance().getInteger("globe.radius"),
-	    InvalidValueException
-	);
+    EXPECT_THROW(
+        Configuration::getInstance().getDouble("globe.name"),
+        InvalidValueException
+    );
 }
