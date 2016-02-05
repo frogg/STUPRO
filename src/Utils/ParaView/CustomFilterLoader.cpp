7936926cdb003446eb0677c24c2b6a733ecdcc01
DasGraMMa <info@papa-schlumpf.net>
2016-02-05 14:16:49 +0100

    Added AStyle-config for VisualStudio extension, formatted these files accordingly

--- src/Utils/ParaView/CustomFilterLoader.cpp
+++ src/Utils/ParaView/CustomFilterLoader.cpp
@@ -12,15 +12,17 @@ void CustomFilterLoader::loadCustomFilters() {
 	QStringList fileList = QDir(customFilterPath).entryList();
 
 	// Get the active proxy manager which will load the filters.
-	vtkSmartPointer<vtkSMSessionProxyManager> proxyMgr = vtkSMProxyManager::GetProxyManager()->GetActiveSessionProxyManager();
+	vtkSmartPointer<vtkSMSessionProxyManager> proxyMgr =
+	    vtkSMProxyManager::GetProxyManager()->GetActiveSessionProxyManager();
 
 	// Declare a pattern-izeable QString beforehand.
 	QString fileNamePath(customFilterPath + "/%1");
 
-	for(auto curFileName : fileList) {
+	for (auto curFileName : fileList) {
 
-		if(curFileName.length() > 2 // Exclude "." and ".."
-		   && (curFileName.endsWith(".xml") || curFileName.endsWith(".cpd"))) { // Only allow xml and cpd files.
+		if (curFileName.length() > 2 // Exclude "." and ".."
+		        && (curFileName.endsWith(".xml")
+		            || curFileName.endsWith(".cpd"))) { // Only allow xml and cpd files.
 
 			// Use the proxy manager to load the custom filters using c-strings.
 			proxyMgr->LoadCustomProxyDefinitions((fileNamePath.arg(curFileName)).toStdString().c_str());
