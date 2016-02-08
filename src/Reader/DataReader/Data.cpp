0e7c86b7bafbeabefa7633c3610b8dc8351682e9
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-08 20:17:36 +0100

    Added an information key to the  class to be used for storing the data type alongside the data that will be passed through the pipeline

--- src/Reader/DataReader/Data.cpp
+++ src/Reader/DataReader/Data.cpp
@@ -1,5 +1,9 @@
 #include <Reader/DataReader/Data.hpp>
 
+#include <vtkInformationIntegerKey.h>
+
+vtkInformationKeyMacro(Data, VTK_DATA_TYPE, DataType);
+
 bool Data::isTemporal(const Data::Type dataType) {
 	return (
 	           dataType == TWEETS || dataType == PRECIPITATION || dataType == TEMPERATURE ||
--- src/Reader/DataReader/Data.hpp
+++ src/Reader/DataReader/Data.hpp
@@ -1,8 +1,18 @@
 #ifndef KRONOS_READER_DATA_HPP
 #define KRONOS_READER_DATA_HPP
 
+#include <Reader/DataReader/vtkInformationDataTypeKey.hpp>
+
+#include <vtkInformationDataObjectMetaDataKey.h>
+
 class Data {
 public:
+	/**
+	 * A VTK information key for storing data type information in an object that will be passed
+	 * through the pipeline.
+	 */
+	static vtkInformationDataTypeKey* VTK_DATA_TYPE();
+	
 	/**
 	 * Enum that denotes a data type. Do not assign values to these. Adding a new data type here
 	 * requires changes to the `JsonValidator` class.
