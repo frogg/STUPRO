1a6aeb64cc04966bfb1c2a3097b95de52810f2fe
zvuv <theilkhz@gmail.com>
2016-02-16 13:11:23 +0100

    Filter can be loaded into Paraview on MIPAS Data. Is curently not shown in the Pipeline Browser

--- src/Filter/SciVisFilter.cpp
+++ src/Filter/SciVisFilter.cpp
@@ -1,5 +1,14 @@
-#include "SciVisFilter.hpp"
+#include "SciVisFilter.h"
 #include "vtkObjectFactory.h"
+#include "vtkPoints.h"
+#include <vtkThreshold.h>
+#include "vtkPoints.h"
+#include "vtkDataSetAlgorithm.h"
+#include <vtkInformation.h>
+#include "vtkInformationVector.h"
+#include <vtkIndent.h>
+#include <vtkDataSet.h>
+#include <vtkUnstructuredGridAlgorithm.h>
 
 vtkStandardNewMacro(SciVisFilter);
 SciVisFilter::SciVisFilter(){}
@@ -9,9 +18,28 @@ SciVisFilter::~SciVisFilter(){}
 
 void SciVisFilter::PrintSelf(ostream& os, vtkIndent indent)
 {
-  this->Superclass::PrintSelf(os,indent);
+  this->Superclass::PrintSelf(os,indent) ;
 }
 
+int	SciVisFilter::RequestData (vtkInformation *request, vtkInformationVector **inputVector, vtkInformationVector *outputVector){
+
+vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+vtkInformation *outInfo = outputVector->GetInformationObject(0);
+
+outInfo=inInfo;
+
+return 1;
+}
+
+int SciVisFilter::FillInputPortInformation (int port, vtkInformation *info){
+this->Superclass::FillInputPortInformation(port,info);
+return 1;
+}
+
+int SciVisFilter::FillOutputPortInformation(int port, vtkInformation *info){
+  info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPointSet");
+  return 1;
+}
 
 
 void SciVisFilter::chooseDate(){
---
+++ src/Filter/SciVisFilter.h
@@ -0,0 +1,43 @@
+#ifndef __SciVisFilter_h
+#define __SciVisFilter_h
+
+#include "vtkPoints.h"
+#include "vtkDataSetAlgorithm.h"
+#include <vtkThreshold.h>
+#include <vtkInformation.h>
+#include "vtkInformationVector.h"
+#include <vtkIndent.h>
+#include <vtkDataSet.h>
+#include <vtkUnstructuredGridAlgorithm.h>
+
+
+
+
+class SciVisFilter  : public vtkThreshold
+{
+	public:
+		static SciVisFilter *New();
+		vtkTypeMacro(SciVisFilter,vtkThreshold);
+		void PrintSelf(ostream& os, vtkIndent indent) override;
+
+
+		void chooseDate();
+
+
+		int	RequestData (vtkInformation *, vtkInformationVector **, vtkInformationVector *) override;
+		int FillInputPortInformation (int port, vtkInformation *info) override;
+		int FillOutputPortInformation(int port, vtkInformation* info) override;
+
+	protected:
+		SciVisFilter();
+		~SciVisFilter();
+
+
+
+	private:
+		SciVisFilter(const SciVisFilter&); //Not implemented
+		void operator=(const SciVisFilter&); //Not implemented
+
+};
+
+#endif
