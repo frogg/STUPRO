3ac3a9124d1fdf98d9ec5c475946cfa78a3a263e
Gravetow <gravetobackup@gmail.com>
2016-02-16 15:25:49 +0100

    Merged the latest dev branch into this branch and removed an illegal function call

--- src/Filter/TwitterHeatmapFilter.cpp
+++ src/Filter/TwitterHeatmapFilter.cpp
@@ -14,15 +14,15 @@
 
 
 TwitterHeatmapFilter::TwitterHeatmapFilter() {
-    //take this transformation, true -> it transforms per default
-    
-    Locator = vtkSmartPointer<vtkPointLocator>::New();
-    
+	//take this transformation, true -> it transforms per default
+
+	Locator = vtkSmartPointer<vtkPointLocator>::New();
+
 }
 
 void TwitterHeatmapFilter::PrintSelf(ostream& os, vtkIndent indent) {
-    this->Superclass::PrintSelf(os, indent);
-    os << indent << "Twitter Filter, Kronos Project" << endl;
+	this->Superclass::PrintSelf(os, indent);
+	os << indent << "Twitter Filter, Kronos Project" << endl;
 }
 
 TwitterHeatmapFilter::~TwitterHeatmapFilter() {
@@ -30,34 +30,34 @@ TwitterHeatmapFilter::~TwitterHeatmapFilter() {
 
 vtkStandardNewMacro(TwitterHeatmapFilter)
 
-int TwitterHeatmapFilter::RequestData(vtkInformation *info,
-                                      vtkInformationVector **inputVector,
-                                      vtkInformationVector *outputVector) {
-    
-    
-    // Get output information from the request vectors
-    vtkInformation* outInfo = outputVector->GetInformationObject(0);
-    
-    // Get the actual objects from the obtained information
-    vtkDataObject* input = vtkDataObject::GetData(inputVector[0]);
-    
-    Locator->SetTolerance(<#double _arg#>)
-    
-    
-    vtkDataObject* output = vtkDataObject::GetData(outputVector);
-    
-    Locator->GetPoints();
-    
-    // Check the input for compatibility
-    if (!input->IsA("vtkPolyData")) {
-        //
-        return 0;
-    }
-    return 1;
+int TwitterHeatmapFilter::RequestData(vtkInformation* info,
+                                      vtkInformationVector** inputVector,
+                                      vtkInformationVector* outputVector) {
+
+
+	// Get output information from the request vectors
+	vtkInformation* outInfo = outputVector->GetInformationObject(0);
+
+	// Get the actual objects from the obtained information
+	vtkDataObject* input = vtkDataObject::GetData(inputVector[0]);
+
+	//Locator->SetTolerance(<#double _arg#>)
+
+
+	vtkDataObject* output = vtkDataObject::GetData(outputVector);
+
+	Locator->GetPoints();
+
+	// Check the input for compatibility
+	if (!input->IsA("vtkPolyData")) {
+		//
+		return 0;
+	}
+	return 1;
 }
 
-int TwitterHeatmapFilter::FillInputPortInformation(int port, vtkInformation *info) {
-    
-    return 1;
+int TwitterHeatmapFilter::FillInputPortInformation(int port, vtkInformation* info) {
+
+	return 1;
 }
 
