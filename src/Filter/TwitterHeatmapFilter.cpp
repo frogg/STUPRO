5669997d0336b2520c4b38ecd8173b07cf2541d8
Gravetow <gravetobackup@gmail.com>
2016-02-19 14:50:29 +0100

    Added a property to the twitterheatmapfilter that allows the user to set the tolerance value of the point locator for data-LOD

--- src/Filter/TwitterHeatmapFilter.cpp
+++ src/Filter/TwitterHeatmapFilter.cpp
@@ -8,7 +8,14 @@
 
 
 #include "TwitterHeatmapFilter.h"
+
+#include "vtkUnstructuredGrid.h"
+#include "vtkCell.h"
+#include "vtkPoints.h"
+#include "vtkPointData.h"
+#include "vtkCellData.h"
 #include "vtkObjectFactory.h"
+
 #include "vtkDataObject.h"
 #include <vtkInformationVector.h>
 #include <vtkInformation.h>
@@ -21,13 +28,26 @@
 #include "vtkPointData.h"
 #include "vtkObjectFactory.h"
 
+#include "vtkIntArray.h"
+#include "vtkCollection.h"
+#include "vtkMergePoints.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+
 
 TwitterHeatmapFilter::TwitterHeatmapFilter() {
 	//take this transformation, true -> it transforms per default
 
 	Locator = vtkSmartPointer<vtkPointLocator>::New();
+	Locator->SetTolerance(toleranceValue);
 
 }
+
+void TwitterHeatmapFilter::setToleranceValue(int newTolerance) {
+	toleranceValue = newTolerance;
+	Locator->SetTolerance(toleranceValue);
+}
+
 
 void TwitterHeatmapFilter::PrintSelf(ostream& os, vtkIndent indent) {
 	this->Superclass::PrintSelf(os, indent);
@@ -35,6 +55,8 @@ void TwitterHeatmapFilter::PrintSelf(ostream& os, vtkIndent indent) {
 }
 
 TwitterHeatmapFilter::~TwitterHeatmapFilter() {
+	this->Locator->Delete();
+	this->Locator = NULL;
 }
 
 vtkStandardNewMacro(TwitterHeatmapFilter)
@@ -43,16 +65,21 @@ int TwitterHeatmapFilter::RequestData(vtkInformation* info,
                                       vtkInformationVector** inputVector,
                                       vtkInformationVector* outputVector) {
 
-	vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
+
+
 	// Get output information from the request vectors
+	vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
 	vtkInformation* outInfo = outputVector->GetInformationObject(0);
-	vtkDataSet* input = vtkDataSet::SafeDownCast(
-	                        inInfo->Get(vtkDataObject::DATA_OBJECT()));
+
 
 	//-------------------------------------------------------
 	// Get the actual objects from the obtained information
-	//vtkDataObject* input = vtkDataObject::GetData(inputVector[0]);
 
+	//vtkDataObject* input = vtkDataObject::GetData(inputVector[0]);
+	vtkDataSet* input = vtkDataSet::SafeDownCast(
+	                        inInfo->Get(vtkDataObject::DATA_OBJECT()));
+	vtkUnstructuredGrid* output = vtkUnstructuredGrid::SafeDownCast(
+	                                  outInfo->Get(vtkDataObject::DATA_OBJECT()));
 	//Locator->SetTolerance(<#double _arg#>)
 
 	//vtkDataObject* output = vtkDataObject::GetData(outputVector);
@@ -65,8 +92,7 @@ int TwitterHeatmapFilter::RequestData(vtkInformation* info,
 	//}
 	//return 1;
 	//-------------------------------------------------------
-	vtkUnstructuredGrid* output = vtkUnstructuredGrid::SafeDownCast(
-	                                  outInfo->Get(vtkDataObject::DATA_OBJECT()));
+
 
 	//if (input->GetNumberOfCells() == 0) {
 	//	// set up a ugrid with same data arrays as input, but
@@ -86,6 +112,21 @@ int TwitterHeatmapFilter::RequestData(vtkInformation* info,
 
 	// First, create a new points array that eliminate duplicate points.
 	// Also create a mapping from the old point id to the new.
+	if (input->GetNumberOfCells() == 0) {
+		// set up a ugrid with same data arrays as input, but
+		// no points, cells or data.
+		output->Allocate(1);
+		output->GetPointData()->CopyAllocate(input->GetPointData());
+		output->GetCellData()->CopyAllocate(input->GetCellData(), 1);
+		vtkPoints* pts = vtkPoints::New();
+		output->SetPoints(pts);
+		pts->Delete();
+		return 1;
+	}
+
+	output->GetPointData()->CopyAllocate(input->GetPointData());
+	output->GetCellData()->PassData(input->GetCellData());
+
 	vtkPoints* newPts = vtkPoints::New();
 	vtkIdType num = input->GetNumberOfPoints();
 	vtkIdType id;
@@ -140,11 +181,13 @@ int TwitterHeatmapFilter::RequestData(vtkInformation* info,
 	cellPoints->Delete();
 	output->Squeeze();
 
+	this->Locator->InitPointInsertion(newPts, input->GetBounds(), num);
+
 	return 1;
 }
 
 int TwitterHeatmapFilter::FillInputPortInformation(int port, vtkInformation* info) {
-
+	info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
 	return 1;
 }
 
