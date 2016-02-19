9ae41a9a82d45a05bd9870fed467490b0a4e9d23
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-19 09:31:45 +0100

    Added a test case for temperature data

--- test/Filter/TestTemporalAggregationFilter.cpp
+++ test/Filter/TestTemporalAggregationFilter.cpp
@@ -8,17 +8,15 @@
 
 #include <vtkTypeFloat32Array.h>
 
-#include <iostream>
-
-TEST(TestTemporalAggregationFilter, TestPrecipitationData) {
+TEST(TestTemporalAggregationFilter, TestTemperatureData) {
     // Read some test data
-	std::unique_ptr<JsonReader> jsonReader = JsonReaderFactory::createReader("res/test-data/temporal-aggregation-test/precipitation-test-data.kJson");
+	std::unique_ptr<JsonReader> jsonReader = JsonReaderFactory::createReader("res/test-data/temporal-aggregation-test/temperature-test-data.kJson");
 	vtkSmartPointer<vtkPolyData> inputDataSet = jsonReader->getVtkDataSet(0);
     
     // Set up the filter and its input
     vtkSmartPointer<TemporalAggregationFilter> filter = TemporalAggregationFilter::New();
     filter->SetInputData(0, inputDataSet);
-    filter->GetInputInformation()->Set(Data::VTK_DATA_TYPE(), Data::PRECIPITATION);
+    filter->GetInputInformation()->Set(Data::VTK_DATA_TYPE(), Data::TEMPERATURE);
     filter->GetInputInformation()->Set(Data::VTK_TIME_RESOLUTION(), 1);
     filter->Update();
 
@@ -28,11 +26,14 @@ TEST(TestTemporalAggregationFilter, TestPrecipitationData) {
 
     ASSERT_TRUE(outputDataSet);
     
-    vtkSmartPointer<vtkDataArray> abstractPrecipitationAmountArray = outputDataSet->GetPointData()
-            ->GetArray("Accumulated Precipitation Amounts");
-    vtkSmartPointer<vtkTypeFloat32Array> precipitationAmountArray = vtkTypeFloat32Array::SafeDownCast(
-                abstractPrecipitationAmountArray);
-    ASSERT_TRUE(precipitationAmountArray);
+    vtkSmartPointer<vtkDataArray> abstractTemperatureArray = outputDataSet->GetPointData()
+            ->GetArray("Average Temperatures");
+    vtkSmartPointer<vtkTypeFloat32Array> temperatureArray = vtkTypeFloat32Array::SafeDownCast(
+                abstractTemperatureArray);
+    ASSERT_TRUE(temperatureArray);
     
-    std::cout << *precipitationAmountArray->GetTuple(0) << std::endl;
+	// Test the data that the filter has produced
+	EXPECT_EQ(2, temperatureArray->GetNumberOfTuples());
+	EXPECT_FLOAT_EQ(8.874, *temperatureArray->GetTuple(0));
+	EXPECT_FLOAT_EQ(16.42, *temperatureArray->GetTuple(1));
 }
