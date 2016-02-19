ba283ea0aa7ee10ad52ead4819fb7b4cf24b286e
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-19 15:25:09 +0100

    Added a function that converts degrees to radians

--- src/Utils/Math/Functions.hpp
+++ src/Utils/Math/Functions.hpp
@@ -1,6 +1,8 @@
 #ifndef KRONOS_MATH_FUNCTIONS_HPP
 #define KRONOS_MATH_FUNCTIONS_HPP
 
+#include <Utils/Misc/Macros.hpp>
+
 /**
  * Returns the positive modulo betwen the two specified numbers.
  *
@@ -12,6 +14,10 @@
 template <typename T>
 T absoluteModulo(T a, T b) {
 	return (a % b + b) % b;
+}
+
+double toRadians(double degrees) {
+	return degrees * (double) (KRONOS_PI / 180);
 }
 
 #endif
