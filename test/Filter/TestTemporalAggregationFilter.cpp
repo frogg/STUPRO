a6001508c298ca272eea3fb3c80b5fa720bfb1ad
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-19 09:47:24 +0100

    Added a test case for wind data

--- test/Filter/TestTemporalAggregationFilter.cpp
+++ test/Filter/TestTemporalAggregationFilter.cpp
@@ -69,3 +69,65 @@ TEST(TestTemporalAggregationFilter, TestCloudCoverageData) {
 	EXPECT_FLOAT_EQ(0.626, *cloudCoverageArray->GetTuple(0));
 	EXPECT_FLOAT_EQ(0.17, *cloudCoverageArray->GetTuple(1));
 }
+
+TEST(TestTemporalAggregationFilter, TestWindData) {
+    // Read some test data
+	std::unique_ptr<JsonReader> jsonReader = JsonReaderFactory::createReader("res/test-data/temporal-aggregation-test/wind-test-data.kJson");
+	vtkSmartPointer<vtkPolyData> inputDataSet = jsonReader->getVtkDataSet(0);
+    
+    // Set up the filter and its input
+    vtkSmartPointer<TemporalAggregationFilter> filter = TemporalAggregationFilter::New();
+    filter->SetInputData(0, inputDataSet);
+    filter->GetInputInformation()->Set(Data::VTK_DATA_TYPE(), Data::WIND);
+    filter->GetInputInformation()->Set(Data::VTK_TIME_RESOLUTION(), 1);
+    filter->Update();
+
+    // Run the filter on the input data
+    vtkSmartPointer<vtkPolyData> outputDataSet = vtkSmartPointer<vtkPolyData>::New();
+    outputDataSet->ShallowCopy(filter->GetPolyDataOutput());
+
+    ASSERT_TRUE(outputDataSet);
+    
+	// Extract the filter's outputs
+    vtkSmartPointer<vtkDataArray> abstractWindSpeedsArray = outputDataSet->GetPointData()
+            ->GetArray("Average Wind Speeds");
+    vtkSmartPointer<vtkTypeFloat32Array> windSpeedsArray = vtkTypeFloat32Array::SafeDownCast(
+                abstractWindSpeedsArray);
+    ASSERT_TRUE(windSpeedsArray);
+	
+	vtkSmartPointer<vtkDataArray> abstractWindDirectionsArray = outputDataSet->GetPointData()
+			->GetArray("Average Wind Directions");
+	vtkSmartPointer<vtkTypeFloat32Array> windDirectionsArray = vtkTypeFloat32Array::SafeDownCast(
+				abstractWindDirectionsArray);
+	ASSERT_TRUE(windDirectionsArray);
+	
+	vtkSmartPointer<vtkDataArray> abstractWindVelocitiesArray = outputDataSet->GetPointData()
+			->GetArray("velocity");
+	vtkSmartPointer<vtkTypeFloat32Array> windVelocitiesArray = vtkTypeFloat32Array::SafeDownCast(
+				abstractWindVelocitiesArray);
+	ASSERT_TRUE(windVelocitiesArray);
+    
+	// Test the data that the filter has produced
+	
+	// First, test the wind speeds
+	EXPECT_EQ(2, windSpeedsArray->GetNumberOfTuples());
+	EXPECT_FLOAT_EQ(0.782, *windSpeedsArray->GetTuple(0));
+	EXPECT_FLOAT_EQ(0.7766667, *windSpeedsArray->GetTuple(1));
+	
+	// Secondly, test the wind directions
+	EXPECT_EQ(2, windDirectionsArray->GetNumberOfTuples());
+	EXPECT_FLOAT_EQ(244, *windDirectionsArray->GetTuple(0));
+	EXPECT_FLOAT_EQ(28, *windDirectionsArray->GetTuple(1));
+	
+	// Finally, test the wind velocities that have been computed from the aforementioned two for flow visualisation
+	EXPECT_EQ(2, windVelocitiesArray->GetNumberOfTuples());
+	EXPECT_EQ(3, windVelocitiesArray->GetNumberOfComponents());
+	
+	EXPECT_FLOAT_EQ(-0.70285702, windVelocitiesArray->GetTuple3(0)[0]);
+	EXPECT_FLOAT_EQ(-0.3428061, windVelocitiesArray->GetTuple3(0)[1]);
+	EXPECT_FLOAT_EQ(0, windVelocitiesArray->GetTuple3(0)[2]);
+	
+	EXPECT_FLOAT_EQ(0.36462292, windVelocitiesArray->GetTuple3(1)[0]);
+	EXPECT_FLOAT_EQ(0.68575597, windVelocitiesArray->GetTuple3(1)[1]);
+	EXPECT_FLOAT_EQ(0, windVelocitiesArray->GetTuple3(1)[2]);
+}
