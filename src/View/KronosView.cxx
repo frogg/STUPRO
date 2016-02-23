5759e5d4ef08fd786735e8d8db205a097de5bd34
DasGraMMa <info@papa-schlumpf.net>
2016-02-23 16:08:58 +0100

    Added dirty workaround to stop the hole

--- src/View/KronosView.cxx
+++ src/View/KronosView.cxx
@@ -3,18 +3,16 @@
 #include <Utils/Misc/MakeUnique.hpp>
 #include <vtkCamera.h>
 #include <vtkCommand.h>
-#include <vtkInformation.h>
 #include <vtkObjectFactory.h>
 #include <KronosView.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkMatrix4x4.h>
+#include <vtkCubeSource.h>
+#include <vtkProperty.h>
 
 vtkStandardNewMacro(KronosView);
 
-void KronosView::Initialize(unsigned int id)
-{
+void KronosView::Initialize(unsigned int id) {
 	this->Superclass::Initialize(id);
 
 	initParameters();
@@ -22,47 +20,57 @@ void KronosView::Initialize(unsigned int id)
 	initGlobe();
 }
 
-void KronosView::initParameters()
-{
+void KronosView::initParameters() {
 	// Initialize parameters.
 	this->displayMode = Globe::DisplayGlobe;
 }
 
-void KronosView::initRenderer()
-{
+void KronosView::initRenderer() {
 	this->cameraModifiedCallback = vtkCallbackCommand::New();
 
-    this->cameraModifiedCallback->SetCallback(
-		[](vtkObject* object, unsigned long eid, void* clientdata, void *calldata)
-		{
-			KronosView * view = (KronosView *)clientdata;
-			if (view->getGlobe()) {
-				view->getGlobe()->onCameraChanged();
-			}
-		});
+	this->cameraModifiedCallback->SetCallback(
+	[](vtkObject * object, unsigned long eid, void* clientdata, void* calldata) {
+		KronosView* view = (KronosView*)clientdata;
+		if (view->getGlobe()) {
+			view->getGlobe()->onCameraChanged();
+		}
+	});
 
-    this->cameraModifiedCallback->SetClientData(this);
+	this->cameraModifiedCallback->SetClientData(this);
 
-	this->GetRenderer()->AddObserver(vtkCommand::ResetCameraClippingRangeEvent, this->cameraModifiedCallback);
+	this->GetRenderer()->AddObserver(vtkCommand::ResetCameraClippingRangeEvent,
+	                                 this->cameraModifiedCallback);
 }
 
-void KronosView::initGlobe()
-{
-	const Configuration & config = Configuration::getInstance();
+void KronosView::initGlobe() {
+	const Configuration& config = Configuration::getInstance();
 
 	// Initialize a unique pointer with a new instance of the Globe
 	// using the current renderer.
 	this->globe = makeUnique<Globe>(*this->GetRenderer());
+
+	// Dirty workaround: Create a transparent box around the globe.
+	vtkSmartPointer<vtkCubeSource> cube = vtkCubeSource::New();
+	cube->SetXLength(2);
+	cube->SetYLength(2);
+	cube->SetZLength(2);
+
+	vtkSmartPointer<vtkPolyDataMapper> cubeMapper = vtkPolyDataMapper::New();
+	cubeMapper->SetInputConnection(cube->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> cubeActor = vtkActor::New();
+	cubeActor->SetMapper(cubeMapper);
+	cubeActor->GetProperty()->SetOpacity(0);
+
+	this->GetRenderer()->AddActor(cubeActor);
 }
 
 
-void KronosView::moveCamera(float latitude, float longitude)
-{
+void KronosView::moveCamera(float latitude, float longitude) {
 	this->moveCamera(latitude, longitude, this->getCameraDistance());
 }
 
-void KronosView::moveCamera(float latitude, float longitude, float distance)
-{
+void KronosView::moveCamera(float latitude, float longitude, float distance) {
 	// left-right, up-down, close-far
 	Vector3d position(0.0, 0.0, 2.6);
 
@@ -87,35 +95,32 @@ void KronosView::moveCamera(float latitude, float longitude, float distance)
 	GetRenderWindow()->Render();
 }
 
-float KronosView::getCameraDistance()
-{
+float KronosView::getCameraDistance() {
 	vtkCamera* cam = GetActiveCamera();
 	double x, y, z;
 	cam->GetPosition(x, y, z);
 	return sqrt(
-		x * x +
-		y * y +
-		z * z
-	);
+	           x * x +
+	           y * y +
+	           z * z
+	       );
 }
 
 
-void KronosView::switchCurrentDisplayMode()
-{
+void KronosView::switchCurrentDisplayMode() {
 	// Invert the display mode and set the interpolation using a static cast.
-	this->displayMode = this->displayMode == Globe::DisplayGlobe ? Globe::DisplayMap : Globe::DisplayGlobe;
+	this->displayMode = this->displayMode == Globe::DisplayGlobe ? Globe::DisplayMap :
+	                    Globe::DisplayGlobe;
 	this->globe->setDisplayMode(this->displayMode);
 
 	// Render the view again.
 	// GetRenderWindow()->Render();
 }
 
-Globe::DisplayMode KronosView::getDisplayMode() const
-{
+Globe::DisplayMode KronosView::getDisplayMode() const {
 	return this->displayMode;
 }
 
-Globe * KronosView::getGlobe() const
-{
+Globe* KronosView::getGlobe() const {
 	return this->globe.get();
 }
