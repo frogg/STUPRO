2a1102f9256fa815dc2f466e7b91492ab64a407f
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-23 16:13:12 +0100

    Added boilerplate code for the temporal interpolation filter

---
+++ src/Filter/TemporalInterpolationFilter.cpp
@@ -0,0 +1,153 @@
+#include <Filter/TemporalInterpolationFilter.h>
+
+#include <vtkPolyData.h>
+#include <vtkPointData.h>
+#include <vtkSmartPointer.h>
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
+#include <vtkObjectFactory.h>
+#include <vtkStreamingDemandDrivenPipeline.h>
+
+vtkStandardNewMacro(TemporalInterpolationFilter);
+
+const QList<Data::Type> TemporalInterpolationFilter::SUPPORTED_DATA_TYPES = QList<Data::Type>() <<
+        Data::PRECIPITATION << Data::TEMPERATURE << Data::WIND << Data::CLOUD_COVERAGE << Data::TWEETS;
+
+TemporalInterpolationFilter::TemporalInterpolationFilter() : error(false), currentTimeStep(0) {
+	this->SetNumberOfInputPorts(1);
+	this->SetNumberOfOutputPorts(1);
+}
+
+TemporalInterpolationFilter::~TemporalInterpolationFilter() { }
+
+void TemporalInterpolationFilter::fail(QString message) {
+	vtkErrorMacro( << message.toStdString());
+	this->error = true;
+}
+
+void TemporalInterpolationFilter::PrintSelf(ostream& os, vtkIndent indent) {
+	this->Superclass::PrintSelf(os, indent);
+
+	os << indent << "Temporal data point interpolation, Kronos Project" << endl;
+}
+
+int TemporalInterpolationFilter::FillInputPortInformation(int port, vtkInformation* info) {
+	if (port == 0) {
+		info->Append(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+		info->Set(vtkAlgorithm::INPUT_IS_OPTIONAL(), 0);
+	}
+
+	return 1;
+}
+
+int TemporalInterpolationFilter::FillOutputPortInformation(int port, vtkInformation* info) {
+	info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPolyData");
+	return 1;
+}
+
+int TemporalInterpolationFilter::RequestInformation (
+    vtkInformation* request,
+    vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) {
+	if (this->error) {
+		return 0;
+	}
+
+	vtkInformation* outInfo = outputVector->GetInformationObject(0);
+	vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
+
+	if (inInfo->Has(Data::VTK_DATA_TYPE())) {
+		this->dataType = static_cast<Data::Type>(inInfo->Get(Data::VTK_DATA_TYPE()));
+	} else {
+		this->fail("This filter only works with data read by the Kronos reader.");
+		return 0;
+	}
+
+	// Overkill code for creating a comma-separated string with the names of all supported data types
+	QString supportedTypes = "";
+	int amountOfSupportedDataTypes = TemporalInterpolationFilter::SUPPORTED_DATA_TYPES.size();
+	if (amountOfSupportedDataTypes == 1) {
+		supportedTypes.append(Data::getDataTypeName(TemporalInterpolationFilter::SUPPORTED_DATA_TYPES.value(
+		                          0)));
+	} else if (amountOfSupportedDataTypes > 1) {
+		for (int i = 0; i < amountOfSupportedDataTypes - 2; i++) {
+			supportedTypes.append(Data::getDataTypeName(TemporalInterpolationFilter::SUPPORTED_DATA_TYPES.value(
+			                          i)));
+			if (i < amountOfSupportedDataTypes - 3) {
+				supportedTypes.append(", ");
+			}
+		}
+		supportedTypes.append(" and ").append(Data::getDataTypeName(
+		        TemporalInterpolationFilter::SUPPORTED_DATA_TYPES.value(amountOfSupportedDataTypes - 1)));
+	}
+
+	if (!TemporalInterpolationFilter::SUPPORTED_DATA_TYPES.contains(this->dataType)) {
+		this->fail(QString("This filter only supports %1 data, but the input contains %2 data.").arg(
+		               supportedTypes, Data::getDataTypeName(this->dataType)));
+		return 0;
+	}
+    
+    // TODO: Check the data's state for compatibility
+
+	return 1;
+}
+
+int TemporalInterpolationFilter::RequestData(
+    vtkInformation* request,
+    vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) {
+	if (this->error) {
+		return 0;
+	}
+
+	vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
+	vtkInformation* outInfo = outputVector->GetInformationObject(0);
+
+	vtkPolyData* input = vtkPolyData::GetData(inInfo);
+	vtkPolyData* output = vtkPolyData::GetData(outInfo);
+
+	if (this->currentTimeStep == 0) {
+		this->SetProgressText("Interpolating data...");
+		this->SetProgress(0.0);
+	}
+
+    std::cout << "Current timestep: " << this->currentTimeStep << std::endl;
+
+	if (this->currentTimeStep < inInfo->Length(vtkStreamingDemandDrivenPipeline::TIME_STEPS())) {
+		// There are still time steps left, continue on
+		request->Set(vtkStreamingDemandDrivenPipeline::CONTINUE_EXECUTING(), 1);
+		this->currentTimeStep++;
+
+		// Update the progress
+		double progress = (double) (this->currentTimeStep / (1.0 * inInfo->Length(
+		                                vtkStreamingDemandDrivenPipeline::TIME_STEPS())));
+		this->UpdateProgress(progress);
+	} else {
+		// The data points have been iterated
+		request->Remove(vtkStreamingDemandDrivenPipeline::CONTINUE_EXECUTING());
+		this->currentTimeStep = 0;
+		this->SetProgressText("");
+		this->SetProgress(1.0);
+	}
+
+	return 1;
+}
+
+int TemporalInterpolationFilter::RequestUpdateExtent (
+    vtkInformation* request,
+    vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) {
+	if (this->error) {
+		return 0;
+	}
+
+	vtkInformation* inputInformation = inputVector[0]->GetInformationObject(0);
+
+	// Make the pipeline executive iterate the upstream pipeline time steps by setting the update time step appropiately
+	double* inputTimeSteps = inputInformation->Get(vtkStreamingDemandDrivenPipeline::TIME_STEPS());
+	if (inputTimeSteps) {
+		inputInformation->Set(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP(), this->currentTimeStep);
+	}
+
+	return 1;
+}
---
+++ src/Filter/TemporalInterpolationFilter.h
@@ -0,0 +1,72 @@
+#ifndef KRONOS_TEMPORAL_INTERPOLATION_FILTER_HPP
+#define KRONOS_TEMPORAL_INTERPOLATION_FILTER_HPP
+
+#include <Reader/DataReader/Data.hpp>
+#include <Utils/Misc/PointCoordinates.hpp>
+
+#include <vtkFiltersGeneralModule.h>
+#include <vtkPassInputTypeAlgorithm.h>
+#include <vtkDataSet.h>
+
+#include <qstring.h>
+#include <qlist.h>
+#include <qmap.h>
+
+class VTKFILTERSGENERAL_EXPORT TemporalInterpolationFilter : public vtkPassInputTypeAlgorithm {
+public:
+    static TemporalInterpolationFilter *New();
+
+    vtkTypeMacro(TemporalInterpolationFilter, vtkPassInputTypeAlgorithm);
+    void PrintSelf(ostream& os, vtkIndent indent);
+
+protected:
+    TemporalInterpolationFilter();
+    ~TemporalInterpolationFilter();
+
+    virtual int FillInputPortInformation(int port, vtkInformation* info);
+    virtual int FillOutputPortInformation(int vtkNotUsed(port), vtkInformation* info);
+
+    virtual int RequestData(vtkInformation *request,
+                          vtkInformationVector **inputVector,
+                          vtkInformationVector *outputVector);
+
+    virtual int RequestUpdateExtent(vtkInformation *request,
+                                  vtkInformationVector **inputVector,
+                                  vtkInformationVector *outputVector);
+    virtual int RequestInformation(vtkInformation *request,
+                                 vtkInformationVector **inputVector,
+                                 vtkInformationVector *outputVector);
+
+private:
+    /**
+     * Display an error message and remember that this filter does not hold valid data.
+     * @param message The error message to be shown to the user
+     */
+    void fail(QString message);
+
+    /**
+     * Boolean flag denoting whether there was an error.
+     */
+    bool error;
+    
+    /**
+     * This integer will store the current time step while this filter iterates through all of them
+     * to aggregate data.
+     */
+    int currentTimeStep;
+    
+    /**
+     * Stores the type of data this filter receives.
+     */
+    Data::Type dataType;
+    
+    /**
+     * This list contains all data types that are supported by this filter.
+     */
+    const static QList<Data::Type> SUPPORTED_DATA_TYPES;
+    
+    TemporalInterpolationFilter(const TemporalInterpolationFilter&); // Not implemented.
+    void operator=(const TemporalInterpolationFilter&); // Not implemented.
+};
+
+#endif
