309cedf2c901d4a466104f96a0adf169e7311cb7
Frederik Riedel <f@polarapps.de>
2016-02-25 12:30:47 +0100

    Style

--- src/Filter/HeatmapDensityFilter.cpp
+++ src/Filter/HeatmapDensityFilter.cpp
@@ -43,20 +43,20 @@
 
 
 HeatmapDensityFilter::HeatmapDensityFilter() {
-    this->SetNumberOfInputPorts(1);
-    this->SetNumberOfOutputPorts(1);
-    
+	this->SetNumberOfInputPorts(1);
+	this->SetNumberOfOutputPorts(1);
+
 }
 
 void HeatmapDensityFilter::setHeatmapResolution(double heatmapResolution) {
-    this->heatmapResolution = heatmapResolution;
-    this->Modified();
+	this->heatmapResolution = heatmapResolution;
+	this->Modified();
 }
 
 
 void HeatmapDensityFilter::PrintSelf(ostream& os, vtkIndent indent) {
-    this->Superclass::PrintSelf(os, indent);
-    os << indent << "Twitter Filter, Kronos Project" << endl;
+	this->Superclass::PrintSelf(os, indent);
+	os << indent << "Twitter Filter, Kronos Project" << endl;
 }
 
 HeatmapDensityFilter::~HeatmapDensityFilter() {}
@@ -68,127 +68,128 @@ vtkStandardNewMacro(HeatmapDensityFilter)
 int HeatmapDensityFilter::RequestData(vtkInformation* info,
                                       vtkInformationVector** inputVector,
                                       vtkInformationVector* outputVector) {
-    
-    
-    
-    
-    
-    
-    // get the input data
-    vtkPolyData* dataInput = vtkPolyData::GetData(inputVector[0],0);
-    
-    double bounds[6];
-    dataInput->GetBounds(bounds);
-    
-    double minX = bounds[0];
-    double maxX = bounds[1];
-    double minY = bounds[2];
-    double maxY = bounds[3];
-    double width = maxX-minX;
-    double height = maxY-minY;
-    
-    
-    
-    int numberOfXComponents = (int) (50.0 * this->heatmapResolution + 0.5);
-    int numberOfYComponents = (int) ((height * numberOfXComponents) / width + 0.5);
-    
-    
-    double stepWidthX = (width / numberOfXComponents);
-    double stepWidthY = (height / numberOfYComponents);
-   
-    
-    
-    
-    
-    
-    vtkPoints* intputDataPoints = dataInput->GetPoints();
-    
-    int numberOfDataInputPoints = intputDataPoints->GetNumberOfPoints();
-
-    std::vector<int> density(numberOfXComponents * numberOfYComponents);
-    
-
-    for(int x = 0; x < numberOfXComponents; x++) {
-        for(int y = 0; y < numberOfYComponents; y++) {
-            
-            int pointID = y*numberOfXComponents + x;
-            density[pointID] = 0.0;
-        }
-    }
-    
-    for(int i=0; i<numberOfDataInputPoints; i++) {
-        
-        double point[3];
-        intputDataPoints->GetPoint(i,point);
-        
-        double relativeX = ((point[0] - minX) / width);
-        double relativeY = ((point[1] - minY) / height);
-        
-        
-        
-        int x = round(relativeX * numberOfXComponents);
-        int y = round(relativeY * numberOfYComponents);
-        
-        std::cout << x << ", " << y << std::endl;
-        
-        int pointID = x*numberOfXComponents + y;
-        
-        density[pointID] = density[pointID]+1;
-    }
-    
-    
-    
-    
-    
-    vtkInformation *outInfo = outputVector->GetInformationObject(0);
-    vtkSmartPointer<vtkPolyData> output = vtkPolyData::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
-    
-    
-    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
-    
-    vtkCellArray* verts = vtkCellArray::New();
-    
-    // Add a cell containing all points. I don't know why we need this, but it doesn't work without.
-    verts->Allocate(verts->EstimateSize(1, numberOfXComponents*numberOfYComponents));
-    verts->InsertNextCell(numberOfXComponents*numberOfYComponents);
-    
-    
-    //we could also use the upper "for loops" to insert these values, but we use a second one to make it more clear to read this code. we trust the compiler to optimize this code properly.
-    for(int x=0; x<numberOfXComponents; x++) {
-        for(int y = 0; y<numberOfYComponents; y++) {
-            double relativeX = minX + x * stepWidthX;
-            double relativeY = minY + y * stepWidthY;
-            verts->InsertCellPoint(points->InsertNextPoint(relativeX,relativeY,0));
-        }
-    }
-    
-    
-    output->SetPoints(points);
-    output->SetVerts(verts);
-    
-    vtkSmartPointer<vtkIntArray> coverageValues = vtkSmartPointer<vtkIntArray>::New();
-    coverageValues->SetNumberOfComponents(1);
-    coverageValues->SetNumberOfValues(density.size());
-    coverageValues->SetName("density");
-    
-    for(int i=0; i< density.size(); i++) {
-        coverageValues->InsertValue(i, density[i]);
-    }
 
 
-    output->GetPointData()->AddArray(coverageValues);
-    
-    return 1;
+
+
+
+
+	// get the input data
+	vtkPolyData* dataInput = vtkPolyData::GetData(inputVector[0], 0);
+
+	double bounds[6];
+	dataInput->GetBounds(bounds);
+
+	double minX = bounds[0];
+	double maxX = bounds[1];
+	double minY = bounds[2];
+	double maxY = bounds[3];
+	double width = maxX - minX;
+	double height = maxY - minY;
+
+
+
+	int numberOfXComponents = (int) (50.0 * this->heatmapResolution + 0.5);
+	int numberOfYComponents = (int) ((height * numberOfXComponents) / width + 0.5);
+
+
+	double stepWidthX = (width / numberOfXComponents);
+	double stepWidthY = (height / numberOfYComponents);
+
+
+
+
+
+
+	vtkPoints* intputDataPoints = dataInput->GetPoints();
+
+	int numberOfDataInputPoints = intputDataPoints->GetNumberOfPoints();
+
+	std::vector<int> density(numberOfXComponents * numberOfYComponents);
+
+
+	for (int x = 0; x < numberOfXComponents; x++) {
+		for (int y = 0; y < numberOfYComponents; y++) {
+
+			int pointID = y * numberOfXComponents + x;
+			density[pointID] = 0.0;
+		}
+	}
+
+	for (int i = 0; i < numberOfDataInputPoints; i++) {
+
+		double point[3];
+		intputDataPoints->GetPoint(i, point);
+
+		double relativeX = ((point[0] - minX) / width);
+		double relativeY = ((point[1] - minY) / height);
+
+
+
+		int x = round(relativeX * numberOfXComponents);
+		int y = round(relativeY * numberOfYComponents);
+
+		std::cout << x << ", " << y << std::endl;
+
+		int pointID = x * numberOfXComponents + y;
+
+		density[pointID] = density[pointID] + 1;
+	}
+
+
+
+
+
+	vtkInformation* outInfo = outputVector->GetInformationObject(0);
+	vtkSmartPointer<vtkPolyData> output = vtkPolyData::SafeDownCast(outInfo->Get(
+	        vtkDataObject::DATA_OBJECT()));
+
+
+	vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+
+	vtkCellArray* verts = vtkCellArray::New();
+
+	// Add a cell containing all points. I don't know why we need this, but it doesn't work without.
+	verts->Allocate(verts->EstimateSize(1, numberOfXComponents * numberOfYComponents));
+	verts->InsertNextCell(numberOfXComponents * numberOfYComponents);
+
+
+	//we could also use the upper "for loops" to insert these values, but we use a second one to make it more clear to read this code. we trust the compiler to optimize this code properly.
+	for (int x = 0; x < numberOfXComponents; x++) {
+		for (int y = 0; y < numberOfYComponents; y++) {
+			double relativeX = minX + x * stepWidthX;
+			double relativeY = minY + y * stepWidthY;
+			verts->InsertCellPoint(points->InsertNextPoint(relativeX, relativeY, 0));
+		}
+	}
+
+
+	output->SetPoints(points);
+	output->SetVerts(verts);
+
+	vtkSmartPointer<vtkIntArray> coverageValues = vtkSmartPointer<vtkIntArray>::New();
+	coverageValues->SetNumberOfComponents(1);
+	coverageValues->SetNumberOfValues(density.size());
+	coverageValues->SetName("density");
+
+	for (int i = 0; i < density.size(); i++) {
+		coverageValues->InsertValue(i, density[i]);
+	}
+
+
+	output->GetPointData()->AddArray(coverageValues);
+
+	return 1;
 }
 
 int HeatmapDensityFilter::FillOutputPortInformation(int port, vtkInformation* info) {
-    info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPolyData");
-    
-    return 1;
+	info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPolyData");
+
+	return 1;
 }
 
 int HeatmapDensityFilter::FillInputPortInformation(int port, vtkInformation* info) {
-    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
-    return 1;
+	info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
+	return 1;
 }
 
