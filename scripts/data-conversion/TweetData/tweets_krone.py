521ba73e938d5f2088671ab9e0d331a6b1e95507
Frederik Riedel <f@polarapps.de>
2016-02-26 16:04:56 +0100

    created valid test data

--- scripts/data-conversion/TweetData/tweets_krone.py
+++ scripts/data-conversion/TweetData/tweets_krone.py
@@ -10,7 +10,7 @@ twitterjsondata = d.read_tabbed_file("Rohdaten_Sandy/numeric_20121022_clean.csv"
 
 tweets = []
 
-
+count = 0
 for tweet in twitterjsondata:
 	
 	try:
@@ -18,19 +18,23 @@ for tweet in twitterjsondata:
 		tweet["longitude"] = float(tweet["lon"])
 		tweet["latitude"] = float(tweet["lat"])
 		tweet["children"] = []
-		tweet["timestamp"] = 0
+		#tweet["timestamp"] = 0
 		tweet["numberOfRetweets"] = 10
-		#timestamp = tweet["timestamp"]
+		
+		timestamp = tweet["timestamp"]
+		#print tweet
 
-		#tweet["timestamp"] = int(time.mktime(datetime.datetime.strptime(str(timestamp), "%Y%m%d%H%M%S").timetuple()))
+		tweet["timestamp"] = int(time.mktime(datetime.datetime.strptime(str(timestamp), "%Y%m%d%H%M%S").timetuple()))
 
 		#print tweet
-		tweets.append(tweet)
+		if count < 10000:
+			tweets.append(tweet)
+		count = count + 1
 		
 
 	except KeyError as e:
 		print "error" + str(e)
 
-newtwitterjsondata = {'meta': {'dataType': 'tweets', 'temporal': True, 'timeResolution': 3600}, 'root': {'children': tweets}}
+newtwitterjsondata = {'meta': {'dataType': 'tweets', 'temporal': True, 'timeResolution': 1}, 'root': {'children': tweets}}
 
 d.write_json_file(newtwitterjsondata, "tweets.kJson", True)
