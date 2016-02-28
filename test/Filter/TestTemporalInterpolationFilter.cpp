d44044e950d86a35f8d510166968de4336aa5b83
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-02-28 21:43:47 +0100

    Added test for wind data

--- test/Filter/TestTemporalInterpolationFilter.cpp
+++ test/Filter/TestTemporalInterpolationFilter.cpp
@@ -2,6 +2,7 @@
 
 #include <Filter/TemporalInterpolationFilter.h>
 #include <Reader/vtkKronosReader.h>
+#include <Utils/Math/Vector3.hpp>
 
 #include <vtkFloatArray.h>
 #include <vtkIntArray.h>
@@ -126,4 +127,90 @@ TEST(TestTemporalInterpolationFilter, TestTemperatureData) {
 	
 	EXPECT_FLOAT_EQ(22.14, *temperatureArray->GetTuple(0));
 	EXPECT_FLOAT_EQ(18.8, *temperatureArray->GetTuple(1));
+}
+
+TEST(TestTemporalInterpolationFilter, TestWindData) {
+	// Read some test data
+	vtkSmartPointer<vtkKronosReader> kronosReader = vtkSmartPointer<vtkKronosReader>::New();
+	kronosReader->SetFileName("res/test-data/temporal-interpolation-test/wind-test-data.kJson");
+
+	// Set up the filter and its input
+	vtkSmartPointer<TemporalInterpolationFilter> filter = TemporalInterpolationFilter::New();
+	filter->SetInputConnection(kronosReader->GetOutputPort());
+	filter->GetInputInformation()->Set(Data::VTK_DATA_TYPE(), Data::TEMPERATURE);
+	filter->GetInputInformation()->Set(Data::VTK_TIME_RESOLUTION(), 1);
+	
+	// Test integral values
+	QList<float> windSpeedsOfFirstPoint = QList<float>() << 0.1 << 1.57 << 0.06 << 1.59;
+	QList<int> windBearingsOfFirstPoint = QList<int>() << 3 << 123 << 207 << 282;
+	QList<Vector3<float>> windVelocitiesOfFirstPoint = QList<Vector3<float>>() << Vector3<float>(0.0052335961, 0.099862956, 0) << Vector3<float>(1.3167129, -0.85508329, 0) << Vector3<float>(-0.027239431, -0.053460389, 0) << Vector3<float>(-1.5553, 0.3306, 0);
+	
+	QList<float> windSpeedsOfSecondPoint = QList<float>() << 0.83 << 0.83 << 0.83 << 0.83;
+	QList<int> windBearingsOfSecondPoint = QList<int>() << 323 << 323 << 323 << 323;
+	QList<Vector3<float>> windVelocitiesOfSecondPoint = QList<Vector3<float>>() << Vector3<float>(-0.49950644, 0.66286749, 0) << Vector3<float>(-0.49950644, 0.66286749, 0) << Vector3<float>(-0.49950644, 0.66286749, 0) << Vector3<float>(-0.49950644, 0.66286749, 0);
+	
+	for (int t = 0; t < windSpeedsOfFirstPoint.size() - 1; t++) {
+		filter->GetOutputInformation(0)->Set(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP(), t);
+		filter->Update();
+
+		// Run the filter on the input data
+		vtkSmartPointer<vtkPolyData> outputDataSet = vtkSmartPointer<vtkPolyData>::New();
+		outputDataSet->ShallowCopy(filter->GetPolyDataOutput());
+
+		ASSERT_TRUE(outputDataSet);
+
+		// Extract the filter's output
+		vtkSmartPointer<vtkFloatArray> windSpeedArray = vtkFloatArray::SafeDownCast(outputDataSet->GetPointData()->GetArray("speeds"));
+		ASSERT_TRUE(windSpeedArray);
+		
+		vtkSmartPointer<vtkFloatArray> windBearingArray = vtkFloatArray::SafeDownCast(outputDataSet->GetPointData()->GetArray("directions"));
+		ASSERT_TRUE(windBearingArray);
+		
+		vtkSmartPointer<vtkFloatArray> windVelocityArray = vtkFloatArray::SafeDownCast(outputDataSet->GetPointData()->GetArray("velocity"));
+		ASSERT_TRUE(windVelocityArray);
+		
+		EXPECT_FLOAT_EQ(windSpeedsOfFirstPoint[t], *windSpeedArray->GetTuple(0));
+		EXPECT_FLOAT_EQ(windBearingsOfFirstPoint[t], *windBearingArray->GetTuple(0));
+		EXPECT_FLOAT_EQ(windVelocitiesOfFirstPoint[t].x, windVelocityArray->GetTuple3(0)[0]);
+		EXPECT_FLOAT_EQ(windVelocitiesOfFirstPoint[t].y, windVelocityArray->GetTuple3(0)[1]);
+		EXPECT_FLOAT_EQ(windVelocitiesOfFirstPoint[t].z, windVelocityArray->GetTuple3(0)[2]);
+		
+		EXPECT_FLOAT_EQ(windSpeedsOfSecondPoint[t], *windSpeedArray->GetTuple(1));
+		EXPECT_FLOAT_EQ(windBearingsOfSecondPoint[t], *windBearingArray->GetTuple(1));
+		EXPECT_FLOAT_EQ(windVelocitiesOfSecondPoint[t].x, windVelocityArray->GetTuple3(1)[0]);
+		EXPECT_FLOAT_EQ(windVelocitiesOfSecondPoint[t].y, windVelocityArray->GetTuple3(1)[1]);
+		EXPECT_FLOAT_EQ(windVelocitiesOfSecondPoint[t].z, windVelocityArray->GetTuple3(1)[2]);
+	}
+	
+	// Test an intermediate value
+	filter->GetOutputInformation(0)->Set(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP(), 0.5);
+	filter->Update();
+
+	// Run the filter on the input data
+	vtkSmartPointer<vtkPolyData> outputDataSet = vtkSmartPointer<vtkPolyData>::New();
+	outputDataSet->ShallowCopy(filter->GetPolyDataOutput());
+
+	ASSERT_TRUE(outputDataSet);
+
+	// Extract the filter's output
+	vtkSmartPointer<vtkFloatArray> windSpeedArray = vtkFloatArray::SafeDownCast(outputDataSet->GetPointData()->GetArray("speeds"));
+	ASSERT_TRUE(windSpeedArray);
+	
+	vtkSmartPointer<vtkFloatArray> windBearingArray = vtkFloatArray::SafeDownCast(outputDataSet->GetPointData()->GetArray("directions"));
+	ASSERT_TRUE(windBearingArray);
+	
+	vtkSmartPointer<vtkFloatArray> windVelocityArray = vtkFloatArray::SafeDownCast(outputDataSet->GetPointData()->GetArray("velocity"));
+	ASSERT_TRUE(windVelocityArray);
+	
+	EXPECT_FLOAT_EQ(0.83, *windSpeedArray->GetTuple(1));
+	EXPECT_FLOAT_EQ(323, *windBearingArray->GetTuple(1));
+	EXPECT_FLOAT_EQ(-0.49950641, windVelocityArray->GetTuple3(1)[0]);
+	EXPECT_FLOAT_EQ(0.66286743, windVelocityArray->GetTuple3(1)[1]);
+	EXPECT_FLOAT_EQ(0, windVelocityArray->GetTuple3(1)[2]);
+	
+	EXPECT_FLOAT_EQ(0.835, *windSpeedArray->GetTuple(0));
+	EXPECT_FLOAT_EQ(63, *windBearingArray->GetTuple(0));
+	EXPECT_FLOAT_EQ(0.74399048, windVelocityArray->GetTuple3(0)[0]);
+	EXPECT_FLOAT_EQ(0.37908211, windVelocityArray->GetTuple3(0)[1]);
+	EXPECT_FLOAT_EQ(0, windVelocityArray->GetTuple3(0)[2]);
 }
