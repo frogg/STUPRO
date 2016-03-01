c3445f219e30b0761dddad5517ded8876c985935
Jonas Auer <jonas.auer.94@gmail.com>
2016-03-02 00:20:26 +0100

    Ran formatter on QtUtils.hpp

--- src/Utils/Misc/QtUtils.hpp
+++ src/Utils/Misc/QtUtils.hpp
@@ -5,13 +5,15 @@
 #include <QEvent>
 
 template <typename F>
-static void postToMainThread(F && function) {
-    struct Event : public QEvent {
-        F function;
-        Event(F && function) : QEvent(QEvent::None), function(std::move(function)) { }
-        ~Event() { function(); }
-    };
-    QCoreApplication::postEvent(QCoreApplication::instance(), new Event(std::move(function)));
+static void postToMainThread(F&& function) {
+	struct Event : public QEvent {
+		F function;
+		Event(F&& function) : QEvent(QEvent::None), function(std::move(function)) { }
+		~Event() {
+			function();
+		}
+	};
+	QCoreApplication::postEvent(QCoreApplication::instance(), new Event(std::move(function)));
 }
 
 #endif // KRONOS_UTILS_MISC_QT_UTILS_HPP
