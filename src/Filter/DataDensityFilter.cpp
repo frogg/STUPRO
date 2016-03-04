b0929041ba34c3109d76ad022523ba944c792299
Jan Tagscherer <jan.tagscherer@gmail.com>
2016-03-04 13:23:26 +0100

    Started refactoring the data density reduction filter

--- src/Filter/DataDensityFilter.cpp
+++ src/Filter/DataDensityFilter.cpp
@@ -1,7 +1,6 @@
 #include <Filter/DataDensityFilter.h>
 
 #include <vtkDataObject.h>
-#include <vtkSmartPointer.h>
 #include <vtkKMeansStatistics.h>
 #include <vtkDoubleArray.h>
 #include <vtkTable.h>
@@ -13,7 +12,6 @@
 
 #include <sstream>
 #include <algorithm>
-#include <Utils/Misc/PointCoordinates.hpp>
 
 //TESTING
 #include <vtkPointData.h>
@@ -24,19 +22,54 @@
 #include <vtkInformationDoubleKey.h>
 
 DataDensityFilter::DataDensityFilter() { }
+DataDensityFilter::~DataDensityFilter() { }
+
+vtkStandardNewMacro(DataDensityFilter)
+
+int DataDensityFilter::FillOutputPortInformation(int port, vtkInformation* info) {
+	info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPolyData");
+	return 1;
+}
+
+int DataDensityFilter::FillInputPortInformation(int port, vtkInformation* info) {
+	info->Remove(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE());
+	info->Append(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+	info->Append(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPointSet");
+	info->Append(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkUnstructuredGrid");
+	return 1;
+}
+
+int DataDensityFilter::RequestData(vtkInformation* info,
+                                   vtkInformationVector** inputVector,
+                                   vtkInformationVector* outputVector) {
+	// Get output information from the request vectors
+	vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
+	vtkInformation* outInfo = outputVector->GetInformationObject(0);
+
+	// Get the actual objects from the obtained information
+	vtkPointSet* input = vtkPointSet::SafeDownCast(inInfo->Get(vtkPolyData::DATA_OBJECT()));
+	vtkPolyData* output = vtkPolyData::SafeDownCast(outInfo->Get(vtkPolyData::DATA_OBJECT()));
+
+	QMap<PointCoordinates, QList<int>> reducedDataPoints;
+	
+	if (kMeansEnabled) {
+		reducedDataPoints = this->reducePointsKMeans(input);
+	} else {
+		reducedDataPoints = this->reducePointsSimple(input);
+	}
+	
+	output->ShallowCopy(this->generateOutputData(reducedDataPoints, input));
+
+	return 1;
+}
 
 void DataDensityFilter::setDataPercentage(double percentage) {
 	this->dataPercentage = percentage;
 	this->Modified();
 }
 
-void DataDensityFilter::setDensityDistance(double distance) {
-	this->densityDistance = distance;
-	this->Modified();
-}
-
-void DataDensityFilter::setKMeansEnabled(bool kMeans) {
-	this->kMeansEnabled = kMeans;
+void DataDensityFilter::setKMeansEnabled(bool enableKMeans) {
+	this->kMeansEnabled = enableKMeans;
 	this->Modified();
 }
 
@@ -45,8 +78,7 @@ void DataDensityFilter::PrintSelf(ostream& os, vtkIndent indent) {
 	os << indent << "Twitter Filter, Kronos Project" << endl;
 }
 
-void DataDensityFilter::KMeansDensityFilter(vtkPolyData* input, vtkPolyData* output) {
-
+QMap<PointCoordinates, QList<int>> DataDensityFilter::reducePointsKMeans(vtkPointSet* input) {
 	// Get the points into the format needed for KMeans
 	vtkSmartPointer<vtkTable> inputData = vtkSmartPointer<vtkTable>::New();
 
@@ -109,13 +141,87 @@ void DataDensityFilter::KMeansDensityFilter(vtkPolyData* input, vtkPolyData* out
 		                          coord2->GetValue(i)));
 	}
 
-	output->SetPoints(centroids);
-	output->SetVerts(vertices);
+	/*output->SetPoints(centroids);
+	output->SetVerts(vertices);*/
+	
+	// TODO: Sort centroids and the points in their respective buckets into the map
+	
+	return QMap<PointCoordinates, QList<int>>();
 }
 
-void DataDensityFilter::NormalFilter(vtkPolyData* input, vtkPolyData* output) {
+QMap<PointCoordinates, QList<int>> DataDensityFilter::reducePointsSimple(vtkPointSet* input) {
+	int numberOfPoints = input->GetNumberOfPoints();
+	QMap<PointCoordinates, QList<int>> outputMap;
+	
+	// Calculate the distance where points should be absorbed by central points
+	double absorptionDistance = 0.0;
+	
+	// Overkill code for calculating the convex hull sphere of the data points
+	double bounds[6];
+	input->GetBounds(bounds);
+	
+	double minX = bounds[0];
+	double maxX = bounds[1];
+	double minY = bounds[2];
+	double maxY = bounds[3];
+	double minZ = bounds[4];
+	double maxZ = bounds[5];
+	double width = maxX - minX;
+	double height = maxY - minY;
+	double depth = maxZ - minZ;
+	
+	PointCoordinates dataSetCenter(minX + (width / 2.0), minY / (height / 2.0), minZ / (depth / 2.0));
 
-	std::vector<int> absorbArray(input->GetNumberOfPoints(), -1);
+	for (int i = 0; i < numberOfPoints; i++) {
+		double pointCoordinatesArray[3];
+		input->GetPoint(i, pointCoordinatesArray);
+		PointCoordinates pointCoordinates(pointCoordinatesArray[0], pointCoordinatesArray[1], pointCoordinatesArray[2]);
+		
+		absorptionDistance = std::max(absorptionDistance, dataSetCenter.getDistanceTo(pointCoordinates));
+	}
+	
+	absorptionDistance *= this->dataPercentage;
+	
+	// This data structure maps a point index to a boolean flag denoting whether it has been absorped by a central point or has become a central point
+	QMap<int, bool> pointAbsorptionStatus;
+	
+	// In the beginning all points are unabsorbed
+	for (int i = 0; i < numberOfPoints; i++) {
+		pointAbsorptionStatus[i] = false;
+	}
+	
+	// Generate central points and assign close points to them
+	for (int i = 0; i < numberOfPoints; i++) {
+		if (pointAbsorptionStatus[i] == false) {
+			double centralPointCoordinatesArray[3];
+			input->GetPoint(i, centralPointCoordinatesArray);
+			PointCoordinates centralPointCoordinates(centralPointCoordinatesArray[0], centralPointCoordinatesArray[1], centralPointCoordinatesArray[2]);
+			QList<int> subordinatePointIndices;
+			
+			pointAbsorptionStatus[i] = true;
+			
+			for (int j = 0; j < numberOfPoints; j++) {
+				if (pointAbsorptionStatus[j] == false) {
+					double subordinatePointCoordinatesArray[3];
+					input->GetPoint(j, subordinatePointCoordinatesArray);
+					PointCoordinates subordinatePointCoordinates(subordinatePointCoordinatesArray[0], subordinatePointCoordinatesArray[1], subordinatePointCoordinatesArray[2]);
+					
+					if (centralPointCoordinates.getDistanceTo(subordinatePointCoordinates) <= absorptionDistance) {
+						subordinatePointIndices.append(j);
+						pointAbsorptionStatus[j] = true;
+					}
+				}
+			}
+			
+			outputMap[centralPointCoordinates] = subordinatePointIndices;
+		}
+	}
+	
+	return outputMap;
+	
+	
+	
+	/*std::vector<int> absorbArray(input->GetNumberOfPoints(), -1);
 	vtkSmartPointer<vtkPoints> resultPoints =
 	    vtkSmartPointer<vtkPoints>::New();
 
@@ -127,11 +233,11 @@ void DataDensityFilter::NormalFilter(vtkPolyData* input, vtkPolyData* output) {
 	tempValues->SetName("temperatures");
 
 
-	/* Get all attribute arrays
+	Get all attribute arrays
 	for (int count = 0; count < input->GetPointData()->GetNumberOfArrays(); count++) {
 		vtkErrorMacro( << "PointData stuff : " << input->GetPointData()->GetArrayName(count));
 	}
-	*/
+	
 
 	for (int r = 0; r < input->GetNumberOfPoints(); r++) {
 		if (absorbArray[r] == -1) {
@@ -144,7 +250,7 @@ void DataDensityFilter::NormalFilter(vtkPolyData* input, vtkPolyData* output) {
 					double o[3];
 					input->GetPoint(j, o);
 					PointCoordinates secondCoords(o[0], o[1], o[2]);
-					if (firstCoords.getDistanceTo(secondCoords) < densityDistance) {
+					if (firstCoords.getDistanceTo(secondCoords) < 10) { // TODO
 						temp = (temp + temperatureArray->GetValue(j)) / 2;
 						absorbArray[j] = 0;
 					}
@@ -165,38 +271,10 @@ void DataDensityFilter::NormalFilter(vtkPolyData* input, vtkPolyData* output) {
 	output->SetPoints(resultPoints);
 	output->SetVerts(vertices);
 	output->GetPointData()->AddArray(tempValues);
+	
+	return QMap<PointCoordinates, QList<int>>();*/
 }
 
-DataDensityFilter::~DataDensityFilter() {}
-
-vtkStandardNewMacro(DataDensityFilter)
-
-int DataDensityFilter::RequestData(vtkInformation* info,
-                                   vtkInformationVector** inputVector,
-                                   vtkInformationVector* outputVector) {
-	// Get output information from the request vectors
-	vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
-	vtkInformation* outInfo = outputVector->GetInformationObject(0);
-
-	// Get the actual objects from the obtained information
-	vtkPolyData* input = vtkPolyData::SafeDownCast(inInfo->Get(vtkPolyData::DATA_OBJECT()));
-	vtkPolyData* output = vtkPolyData::SafeDownCast(outInfo->Get(vtkPolyData::DATA_OBJECT()));
-
-	if (kMeansEnabled) {
-		KMeansDensityFilter(input, output);
-	} else {
-		NormalFilter(input, output);
-	}
-
-	return 1;
-}
-
-int DataDensityFilter::FillOutputPortInformation(int port, vtkInformation* info) {
-	info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPolyData");
-	return 1;
-}
-
-int DataDensityFilter::FillInputPortInformation(int port, vtkInformation* info) {
-	info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
-	return 1;
+vtkSmartPointer<vtkPolyData> DataDensityFilter::generateOutputData(QMap<PointCoordinates, QList<int>> centralPoints, vtkPointSet* input) {
+	
 }
--- src/Filter/DataDensityFilter.h
+++ src/Filter/DataDensityFilter.h
@@ -5,6 +5,11 @@
 
 #include <vtkDataObjectAlgorithm.h>
 #include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+
+#include <qmap.h>
+#include <qlist.h>
+#include <Utils/Misc/PointCoordinates.hpp>
 
 class DataDensityFilter : public vtkDataObjectAlgorithm {
 
@@ -18,18 +23,17 @@ public:
 	 * @param percentage The new percentage of points to keep
 	 */
 	void setDataPercentage(double percentage);
-	void setDensityDistance(double distance);
+	
+	/**
+	 * Callback method for setting whether the kMeans algorithm should be used.
+	 * @param enableKMeans True if kMeans should be used, false otherwise
+	 */
 	void setKMeansEnabled(bool enableKMeans);
 
 protected:
-
-	void KMeansDensityFilter(vtkPolyData* input, vtkPolyData* output);
-	void NormalFilter(vtkPolyData* input, vtkPolyData* output);
-
 	int RequestData(vtkInformation* info,
 	                vtkInformationVector** inputVector,
 	                vtkInformationVector* outputVector) override;
-
 
 	int FillInputPortInformation(int port, vtkInformation* info) override;
 	int FillOutputPortInformation(int port, vtkInformation* info) override;
@@ -43,6 +47,28 @@ private:
 
 	DataDensityFilter(const DataDensityFilter&);  // Not implemented.
 	void operator=(const DataDensityFilter&);  // Not implemented.
+	
+	/**
+	 * Reduce a set of points using the kMeans algorithm.
+	 * @param input The input point set
+	 * @return A map of point coordinates of central points and a list of point IDs that belong to them
+	 */
+	QMap<PointCoordinates, QList<int>> reducePointsKMeans(vtkPointSet* input);
+	
+	/**
+	 * Reduce a set of points using a simple approach.
+	 * @param input The input point set
+	 * @return A map of point coordinates of central points and a list of point IDs that belong to them
+	 */
+	QMap<PointCoordinates, QList<int>> reducePointsSimple(vtkPointSet* input);
+	
+	/**
+	 * Output poly data from a map of central point coordinates, their associated subordinate points and all point data.
+	 * @param centralPoints A map of point coordinates of central points and a list of point IDs that belong to them
+	 * @param input The input point set
+	 * @return A poly data object with all central points and averaged scalar values
+	 */
+	vtkSmartPointer<vtkPolyData> generateOutputData(QMap<PointCoordinates, QList<int>> centralPoints, vtkPointSet* input);
 
 	/**
 	 * The percentage of points that should be kept. 1 refers to keeping all points, 0 means keeping one point.
@@ -50,8 +76,9 @@ private:
 	 */
 	double dataPercentage;
 
-	double densityDistance;
-
+	/**
+	 * Boolean flag denoting whether the kMeans algorithm should be used.
+	 */
 	bool kMeansEnabled;
 };
 
