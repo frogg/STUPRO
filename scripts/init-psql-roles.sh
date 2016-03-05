bb70ee2046b14ae3f0dc7057dbe92b9db8c96c10
Frederik Riedel <f@polarapps.de>
2016-03-06 00:52:58 +0100

    added documents folder + added Modultestplan.pdf

---
+++ scripts/init-psql-roles.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+USERNAME="stuproUser"
+PASSWORD="weloveparaview"
+DB_NAME="kronos"
+
+# create a user with the privilege to create databases and without superuser privileges
+createuser $USERNAME --createdb --echo --login --no-inherit --no-superuser --no-replication
+
+# set a password for the user
+echo "alter role \"$USERNAME\" with password '$PASSWORD';" | psql -U $USERNAME template1
+
+# create a database belonging to the user
+echo "create database \"$DB_NAME\" with owner \"$USERNAME\" encoding 'UTF8' template template0;" | psql -U $USERNAME template1
+
+python ./DBCity.py
